5c124138b72e2e11868e07e75a36e924ad0dcd33
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-05-30 16:17:20 +0700

    fix

--- app/controllers/affiches_controller.rb
+++ app/controllers/affiches_controller.rb
@@ -11,7 +11,7 @@ class AffichesController < InheritedResourcesController
   def index
     if request.xhr?
       render :text => '<div class="empty">Ничего не найдено ;(</div>', :layout => false and return if collection.empty?
-      render :partial => 'commons/list', :locals => { :collection => collection }, :layout => false and return
+      render :partial => 'commons/list', :locals => { :collection => collection, :remote => true }, :layout => false and return
     end
 
     index!
--- app/controllers/eatings_controller.rb
+++ app/controllers/eatings_controller.rb
@@ -12,7 +12,7 @@ class EatingsController < InheritedResourcesController
   def index
     if request.xhr?
       render :text => '<div class="empty">Ничего не найдено ;(</div>', :layout => false and return if collection.empty?
-      render :partial => 'commons/list', :locals => { :collection => collection }, :layout => false and return
+      render :partial => 'commons/list', :locals => { :collection => collection, :remote => true }, :layout => false and return
     end
 
     index!
