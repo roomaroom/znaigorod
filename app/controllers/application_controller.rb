6d2c90464e81ccd7605524dc63a0f4a395cc8350
Dmitry Lihachev <lda@openteam.ru>
2012-06-01 09:51:24 +0700

    баннеры подбора заведений на главной теперь с развлечениями

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,31 @@
+# encoding: utf-8
+
 class ApplicationController < ActionController::Base
-  helper_method :banners
+  helper_method :banners, :hot_offers
 
   layout 'public'
 
   protect_from_forgery
 
   protected
+    def hot_offers
+      all_offers[-6..-1]
+    end
+
+    def all_offers
+      [
+        'eatings/Пиццерии',
+        'eatings/Завтраки',
+        'eatings/Бизнес-Ланч',
+        'eatings/Детские Кафе',
+        'funnies/Бильярдные Залы',
+        'eatings/Столовые',
+        'eatings/Кофейни',
+        'eatings/Wi-Fi',
+        'funnies/Боулинг',
+      ]
+    end
+
     def banners
       Affiche.with_images.with_showings.latest(4)
     end
