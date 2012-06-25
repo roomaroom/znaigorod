fc5da1a25dfba20b9b23c8c44ee5148aaee1443a
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-06-25 09:02:36 +0700

    Search by actual or gone affiches for manage

--- app/controllers/manage/affiches_controller.rb
+++ app/controllers/manage/affiches_controller.rb
@@ -5,6 +5,26 @@ class Manage::AffichesController < Manage::ApplicationController
 
   protected
     def collection
+      params[:include_gone] ? include_gone : only_actual
+    end
+
+    def only_actual
+      showing_ids = ShowingSearch.new(params[:search]).result_ids
+
+      Affiche.search {
+        keywords(params[:q])
+        # NOTE: use [0] if showing_ids is empty
+        with(:showing_ids, showing_ids + [0])
+        paginate(paginate_options.merge(:per_page => 30))
+
+        adjust_solr_params do |params|
+          params[:sort] = 'recip(abs(ms(NOW,first_showing_time_dt)),3.16e-11,1,1) desc'
+        end
+
+      }.results
+    end
+
+    def include_gone
       @search ||= Sunspot.search(Affiche) do
         keywords(params[:q])
         paginate(:page => params[:page], :per_page => 20)
