45b91d443e72e52424a196b179a5ad4aadd2cb61
Evgeny Lapin <lev@openteam.ru>
2012-06-28 13:23:28 +0700

    revert redcloth vs. gilensize

--- app/controllers/manage/red_cloth_controller.rb
+++ app/controllers/manage/red_cloth_controller.rb
@@ -1,6 +1,6 @@
 class Manage::RedClothController < Manage::ApplicationController
   def show
-    html = params[:text] ? RedCloth.new(params[:text]).gilensize.to_html : ''
+    html = params[:text] ? RedCloth.new(params[:text]).to_html : ''
 
     render :text => html
   end
