d2e5cd9efaa9f1a54ab5f54ee9b3f1c4cb62d6b5
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-07-12 11:28:14 +0700

    Render attachments and images in public

---
+++ app/controllers/manage/attachments_controller.rb
@@ -0,0 +1,26 @@
+class Manage::AttachmentsController < Manage::ApplicationController
+  actions :new, :create, :destroy
+
+  belongs_to :affiche, :organization, :polymorphic => true, :optional => true
+
+  def create
+    create! do
+      is_parent_affiche? ? manage_affiche_path(parent) : manage_organization_path(parent)
+    end
+  end
+
+  def destroy
+    destroy! do
+      is_parent_affiche? ? manage_affiche_path(parent) : manage_organization_path(parent)
+    end
+  end
+
+  protected
+    def collection_path
+      is_parent_affiche? ? manage_affiche_attachments_path(parent) : manage_organization_attachments_path(parent)
+    end
+
+    def is_parent_affiche?
+      parent.class.superclass.name.underscore == 'affiche'
+    end
+end
