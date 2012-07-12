045b8c717600847c23c3fd2daf0effa0d38dfe2b
tab <tukhfatullin@gmail.com>
2012-07-12 08:33:29 +0700

    Fix Css and Layout in Manage

--- config/manage_navigation.rb
+++ config/manage_navigation.rb
@@ -2,6 +2,7 @@
 # Configures your navigation
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
+  	primary.item :manage, 'ЗнайГород'
     primary.item :organisations, 'Список заведений', manage_organizations_path, :highlights_on => ->(){ controller_name == 'organizations' && action_name == 'index' }
     primary.item :add_organization, 'Добавить заведение', new_manage_organization_path, :highlights_on => ->(){ controller_name == 'organizations' && %w(create new).include?(action_name) }
     primary.item :affiches, 'Список мероприятий', manage_affiches_path, :highlights_on => ->(){ controller_name == 'affiches' && action_name == 'index' }
