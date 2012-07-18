068c767ef063824e3b5c438b55ac62ebee868a8a
Manin Iliy <bkmzvf@gmail.com>
2012-07-18 15:29:32 +0700

    fix building object

--- app/controllers/manage/exhibitions_controller.rb
+++ app/controllers/manage/exhibitions_controller.rb
@@ -2,10 +2,4 @@ class Manage::ExhibitionsController < Manage::ApplicationController
   actions :all, :except => :show
 
   has_scope :page, :default => 1
-
-  def build_resource
-    super
-    resource.build_affiche_schedule unless resource.affiche_schedule && resource.showings.any?
-    resource
-  end
 end
