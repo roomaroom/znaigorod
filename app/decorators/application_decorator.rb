932a3c99309e2e2a96702d226e740ba09524efa1
Evgeny Lapin <lev@openteam.ru>
2012-09-17 10:32:14 +0700

    style of list organizations

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -9,6 +9,7 @@ class ApplicationDecorator < Draper::Base
 
 
   def resized_image_url(url, width, height, crop = true)
+    return "" if url.blank?
     image_url, image_id, image_width, image_height, image_crop, image_filename =
       url.match(%r{(.*)/files/(\d+)/(?:(\d+)-(\d+)(\!)?/)?(.*)})[1..-1]
 
