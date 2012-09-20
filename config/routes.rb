e93ea150c7a65f1c898ee6b6c1db6f697f2170c5
Dmitry Lihachev <lda@openteam.ru>
2012-09-20 13:03:20 +0700

    ожили ссылка на галерейки

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ Znaigorod::Application.routes.draw do
       :kind => /movies|concerts|parties|spectacles|exhibitions|sportsevents|others|affiches/,
       :period => /today|weekly|weekend|all|daily/, :as => :affiches
 
-  get 'photogalleries/:period' => 'photogalleries#index', period: /all|month|week/
+  get 'photogalleries/:period' => 'photogalleries#index', period: /all|month|week/, :as => :photogalleries
 
   Affiche.descendants.each do |type|
     get "#{type.name.downcase}/:id" => 'affiches#show', :as => "#{type.name.downcase}"
