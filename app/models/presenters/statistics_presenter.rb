ef9dbe6c5dbf024d8863500b492903a95e04ca25
Dmitry Lihachev <lda@openteam.ru>
2012-09-27 15:20:37 +0700

    little fix

--- app/models/presenters/statistics_presenter.rb
+++ app/models/presenters/statistics_presenter.rb
@@ -1,7 +1,7 @@
 class StatisticsPresenter
   def affiches_with_page_views
     Affiche.unscoped.where('yandex_metrika_page_views IS NOT NULL').order('yandex_metrika_page_views DESC').
-      where(:created_at => [Date.today - 1.month..Date.today]).
+      where('created_at >= ?', from_date).
       limit(10).
       map { |a|
       AfficheDecorator.new(a)
@@ -10,10 +10,14 @@ class StatisticsPresenter
 
   def affiches_with_likes
     Affiche.unscoped.where('vkontakte_likes IS NOT NULL').order('vkontakte_likes DESC').
-      where(:created_at => [Date.today - 1.month..Date.today]).
+      where('created_at >= ?', from_date).
       limit(10).
       map { |a|
       AfficheDecorator.new(a)
     }
+  end
+
+  def from_date
+    1.month.ago
   end
 end
