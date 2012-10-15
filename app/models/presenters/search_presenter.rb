bc54f226d3923def7c634bb08830639c0fe2c881
Dmitriy Baraksanov <bdn@openteam.ru>
2012-10-15 22:13:44 +0700

    количество результатов в презентере

--- app/models/presenters/search_presenter.rb
+++ app/models/presenters/search_presenter.rb
@@ -11,6 +11,10 @@ class SearchPresenter
     @searcher ||= HasSearcher.searcher(:global, params)
   end
 
+  def total
+    searcher.total
+  end
+
   def collection
     @collection ||= searcher.paginate(page: page, per_page: 10)
   end
