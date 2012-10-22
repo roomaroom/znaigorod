b297b112135b3ba9555491c5a0fbe153c39a5e05
Evgeny Lapin <lev@openteam.ru>
2012-10-22 17:10:25 +0700

    Порядок полей на форме

--- app/models/saunas/sauna_accessory.rb
+++ app/models/saunas/sauna_accessory.rb
@@ -2,7 +2,7 @@ class SaunaAccessory < ActiveRecord::Base
   include UsefulAttributes
 
   attr_accessible :sheets, :sneakers, :bathrobes, :towels, :oils,
-                  :ability_oils, :ability_own_alcohol, :alcohol_for_sale
+                  :ability_oils, :ability_own_alcohol, :alcohol_for_sale, :ware
 
   belongs_to :sauna
 end
