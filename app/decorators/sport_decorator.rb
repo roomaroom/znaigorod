9666ab8a3338b0730d9c68d938dbcfcef47e9a1e
Evgeny Lapin <lev@openteam.ru>
2012-11-16 15:51:58 +0700

    fix sport decorator & add styles for it

--- app/decorators/sport_decorator.rb
+++ app/decorators/sport_decorator.rb
@@ -8,20 +8,24 @@ class SportDecorator < SuborganizationDecorator
   end
 
   def characteristics_on_show
-    content = ""
-    #content << "\n"
-    #self.send(name).each do |value|
-    #end
+
+    content = "<tr>" +
+              "<th>Предложения</th>" +
+              "<th>Возраст</th>" +
+              "</tr>\n"
+
     services.group_by(&:title).each do |title, services|
-      content << h.content_tag(:li, title, class: "title")
+      content << "<tr>"
+      content << h.content_tag(:td, title, class: "title", colspan: 2)
+      content << "</tr>\n"
       services.each do |service|
-        content << h.content_tag(:li, service.offer)
-        content << h.content_tag(:li, service.feature)
-        content << h.content_tag(:li, service.age)
+        content << "<tr>"
+        content << h.content_tag(:td, h.raw("#{h.content_tag(:p, service.offer, class: :offer)}\n#{h.content_tag(:p, service.feature, class: :feature)}"))
+        content << h.content_tag(:td, service.age)
+        content << "</tr>\n"
       end
-      content << "\n"
     end
-    h.content_tag(:ul, content.html_safe) + characteristics_by_type("features offers")
+    h.content_tag(:table, content.html_safe, class: :services_attributes) + characteristics_by_type("features offers")
   end
 
 end
