c1700e8a0301ba8aff483b83ee97c40276bb4fb9
Dmitriy Baraksanov <bdn@openteam.ru>
2012-11-22 10:22:35 +0700

    поправлено отображение информации о спортивных и творческих заведениях и нормальное название заведения в списке на менеждере

--- app/decorators/creation_decorator.rb
+++ app/decorators/creation_decorator.rb
@@ -4,7 +4,7 @@ class CreationDecorator < SuborganizationDecorator
   decorates :creation
 
   def characteristics_on_list
-    characteristics_by_type("features offers")
+    characteristics_by_type("features")
   end
 
   def characteristics_on_show
@@ -14,17 +14,17 @@ class CreationDecorator < SuborganizationDecorator
               "<th>Возраст</th>" +
               "</tr>\n"
 
-    services.group_by(&:title).each do |title, services|
+    services.group_by(&:category).each do |category, services|
       content << "<tr>"
-      content << h.content_tag(:td, title, class: "title", colspan: 2)
+      content << h.content_tag(:td, category, class: "title", colspan: 2)
       content << "</tr>\n"
       services.each do |service|
         content << "<tr>"
-        content << h.content_tag(:td, h.raw("#{h.content_tag(:p, service.offer, class: :offer)}\n#{h.content_tag(:p, service.feature, class: :feature)}"))
+        content << h.content_tag(:td, h.raw("#{h.content_tag(:p, service.title, class: :offer)}\n#{service.description.as_html}"))
         content << h.content_tag(:td, service.age)
         content << "</tr>\n"
       end
     end
-    h.content_tag(:table, content.html_safe, class: :services_attributes) + characteristics_by_type("features offers")
+    h.content_tag(:table, content.html_safe, class: :services_attributes) + characteristics_by_type("features")
   end
 end
