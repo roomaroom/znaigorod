fa4ea7d8e6031761922fd80784503207d17ad238
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-11-28 10:30:46 +0700

    Set cases for suborganization attributes

---
+++ db/migrate/20121128024329_set_right_case_for_suborganization_attributes.rb
@@ -0,0 +1,24 @@
+class SetRightCaseForSuborganizationAttributes < ActiveRecord::Migration
+  def models
+    @models ||= Organization.new.available_suborganization_kinds.map { |kind| kind.classify.constantize }
+  end
+
+  def up
+    models.each do |model|
+      puts model.name
+      pb = ProgressBar.new(model.count)
+
+      model.find_each do |record|
+        record.update_attributes! :category => (record.category || '').split(',').map(&:squish).map(&:mb_chars).map(&:capitalize).join(', ') if record.has_attribute?(:category)
+        record.update_attributes! :cuisine  => (record.cuisine || '').split(',').map(&:squish).map(&:mb_chars).map(&:capitalize).join(', ')  if record.has_attribute?(:cuisine)
+        record.update_attributes! :offer    => (record.offer || '').split(',').map(&:squish).map(&:mb_chars).map(&:downcase).join(', ')      if record.has_attribute?(:offer)
+        record.update_attributes! :feature  => (record.feature || '').split(',').map(&:squish).map(&:mb_chars).map(&:downcase).join(', ')    if record.has_attribute?(:feature)
+
+        pb.increment!
+      end
+    end
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121127053016) do
+ActiveRecord::Schema.define(:version => 20121128024329) do
 
   create_table "addresses", :force => true do |t|
     t.string   "street"
