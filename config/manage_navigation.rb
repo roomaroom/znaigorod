b298cd31e7440ab7c65c2d5cc15cc0867e88040a
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-12-27 12:06:04 +0700

    Completed managment of posts

--- config/manage_navigation.rb
+++ config/manage_navigation.rb
@@ -5,6 +5,7 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.item :organisations, 'Заведения города', manage_organizations_path, :highlights_on => ->(){ controller_name == 'organizations' || resource_class.try(:superclass) == Organization }
   	primary.item :main_page, 'ЗнайГород', root_url
     primary.item :affiches, 'Мероприятия города', manage_affiches_path, :highlights_on => ->(){ controller_name == 'affiches' || resource_class.try(:superclass) == Affiche }
+    primary.item :posts, 'Посты города', manage_posts_path, :highlights_on => ->(){ controller_name == 'posts' || resource_class.try(:superclass) == Post }
     primary.dom_class = 'navigation'
   end
 end
