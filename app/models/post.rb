2c7506dfe85ad870a6982d93ccc9e9aebfddc321
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-12-27 09:43:50 +0700

    Add Post model and initial CRUD

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   extend FriendlyId
+<<<<<<< HEAD
 
   attr_accessible :annotation, :content, :poster_url, :title, :vfs_path
 
@@ -10,4 +11,13 @@ class Post < ActiveRecord::Base
   def self.generate_vfs_path
     "/znaigorod/posts/#{Time.now.strftime('%Y/%m/%d/%H-%M')}-#{SecureRandom.hex(4)}"
   end
+=======
+  attr_accessible :annotation, :content, :poster_url, :title, :vfs_path
+
+  validates_presence_of :annotation
+  validates_presence_of :content
+  validates_presence_of :title
+
+  friendly_id :title, use: :slugged
+>>>>>>> Add Post model and initial CRUD
 end
