7394c86bb919eccb0f9de028380315454409be9a
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-12-29 10:26:31 +0700

    HOTTEST FIX

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ActiveRecord::Base
   extend FriendlyId
-<<<<<<< HEAD
 
   attr_accessible :annotation, :content, :poster_url, :title, :vfs_path
 
@@ -11,13 +10,4 @@ class Post < ActiveRecord::Base
   def self.generate_vfs_path
     "/znaigorod/posts/#{Time.now.strftime('%Y/%m/%d/%H-%M')}-#{SecureRandom.hex(4)}"
   end
-=======
-  attr_accessible :annotation, :content, :poster_url, :title, :vfs_path
-
-  validates_presence_of :annotation
-  validates_presence_of :content
-  validates_presence_of :title
-
-  friendly_id :title, use: :slugged
->>>>>>> Add Post model and initial CRUD
 end
