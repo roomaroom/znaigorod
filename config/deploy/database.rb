e2705bfdbb7acc33c2f65537d88b72ac7d73cced
Manin Iliy <bkmzvf@gmail.com>
2013-01-18 13:37:25 +0700

    remove export task

--- config/deploy/database.rb
+++ config/deploy/database.rb
@@ -15,20 +15,6 @@ local_db_username = config[local_rails_env]["username"]
 
 set :timestamp, Time.now.strftime("%Y-%m-%d-%H-%M")
 namespace :db do
-  desc "upload local database to remote server"
-  task :export do
-    if adapter == "postgresql"
-      run_locally("pg_dump -O #{local_database} > tmp/#{local_database}-#{timestamp}.sql")
-      upload "tmp/#{local_database}-#{timestamp}.sql", "#{deploy_to}/shared/database/#{local_database}-#{timestamp}.sql"
-      sudo "/etc/init.d/#{unicorn_instance_name} stop"
-      run "cd #{deploy_to}/current && RAILS_ENV=production bin/rake db:drop && RAILS_ENV=production bin/rake db:create"
-      run "psql -d #{database} -h #{host} -U #{db_username} -f #{deploy_to}/shared/database/#{local_database}-#{timestamp}.sql"
-      sudo "/etc/init.d/#{unicorn_instance_name} start"
-    else
-      puts "Cannot backup, adapter #{adapter} is not implemented for backup yet"
-    end
-  end
-
   desc "download data to local database"
   task :import do
     run_locally("bin/rake sunspot:solr:stop; true")
