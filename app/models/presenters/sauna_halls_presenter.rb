fe0c2e9d0e642e87d612bb7db2d0a5151a33cf43
Manin Iliy <bkmzvf@gmail.com>
2013-01-28 12:26:38 +0700

    +подсказка

--- app/models/presenters/sauna_halls_presenter.rb
+++ app/models/presenters/sauna_halls_presenter.rb
@@ -174,8 +174,9 @@ class SaunaHallsPresenter
   def distance_sort_link
     html_options = { class: 'disabled distance' } unless geo_filter_used?
     html_options = { class: 'selected distance' } if order_by_distance? && geo_filter_used?
+    html_options.merge! :title => 'Не активно, если не определено ваше местоположение.'
 
-    content_tag :li, link_to(I18n.t('sauna.sort.distance'), saunas_path(params.merge(order_by: 'distance'), :title => 'Не активно, если не определено местоположение.'), html_options)
+    content_tag :li, link_to( I18n.t('sauna.sort.distance'), saunas_path(params.merge(order_by: 'distance')), html_options)
   end
 
   def sort_links
