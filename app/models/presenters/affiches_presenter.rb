bc0c7afadb8b0dff8e2f6bc4fee6f3d4899718d6
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-02-15 14:53:02 +0700

    Real values for price

--- app/models/presenters/affiches_presenter.rb
+++ app/models/presenters/affiches_presenter.rb
@@ -1,10 +1,15 @@
 # encoding: utf-8
 
 class AffichesPresenter
-  include ActiveAttr::MassAssignment
+  attr_accessor :price_min, :price_max,
+                :time_from, :time_to
 
   def initialize(args = {})
-    super(args)
+    self.price_min = self.price_min.blank? ? available_price_min : self.price_min
+    self.price_max = self.price_max.blank? ? available_price_max : self.price_max
+
+    self.time_from = self.time_from.blank? ? 0  : self.time_from
+    self.time_to   = self.time_to.blank?   ? 23 : self.time_to
   end
 
   def sort_links
@@ -19,10 +24,18 @@ class AffichesPresenter
     collection.size
   end
 
+  def available_price_min
+    Affiche.with_showings.joins(:showings).minimum(:price_min)
+  end
+
+  def available_price_max
+    Affiche.with_showings.joins(:showings).maximum(:price_max)
+  end
+
   def price
     Hashie::Mash.new(
-      available: { minimum: 0, maximum: 2500 },
-      selected: { minimum: 200, maximum: 500 }
+      available: { minimum: available_price_min, maximum: available_price_max },
+      selected: { minimum: price_min, maximum: price_max }
     )
   end
 
@@ -32,8 +45,8 @@ class AffichesPresenter
 
   def time
     Hashie::Mash.new(
-      available: { minimum: 0, maximum: 23 },
-      selected: { minimum: 10, maximum: 20 }
+      available: { from: 0, to: 23 },
+      selected: { from: 10, to: 20 }
     )
   end
 
