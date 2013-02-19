359a1a5b54c3798c7d53f3916b468bd8f00d5590
Evgeny Lapin <lev@openteam.ru>
2013-02-19 19:25:16 +0700

    301 Moved Permanently for /popular. again

--- config/routes.rb
+++ config/routes.rb
@@ -132,6 +132,6 @@ Znaigorod::Application.routes.draw do
     "/#{a.class.model_name.downcase}/#{a.slug}"
   }
 
-  match '/popular', :to => redirect('application#main_page')
+  match '/popular', :to => redirect('/')
 
 end
