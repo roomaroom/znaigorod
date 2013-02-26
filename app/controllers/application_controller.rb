21123134b4ceeb2cecaa8732c221458d4ba38cb8
Evgeny Lapin <lev@openteam.ru>
2013-02-26 13:17:30 +0700

    switch layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   include UrlHelper
   helper_method :banners, :hot_offers, :page, :per_page
 
-  layout 'public'
+  layout 'contests/spring_mood'
 
   def main_page
     @affiche_today = AfficheToday.new 'movies'
