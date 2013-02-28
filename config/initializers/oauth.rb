53993e1722213705355fe6761991df908b971f08
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-02-28 11:03:42 +0700

    Use different keys for VK

--- config/initializers/oauth.rb
+++ config/initializers/oauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :vkontakte, Settings[:vk][:app_id], Settings[:vk][:app_secret]
+  use = Settings[:vk][:use]
+
+  provider :vkontakte, Settings[:vk][use][:app_id], Settings[:vk][use][:app_secret]
 end
