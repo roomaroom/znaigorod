75b0790209262079482d72863a9b360913b098b6
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-03-06 14:19:15 +0700

    Mark sortings link

--- app/presenters/showings/showings_presenter.rb
+++ app/presenters/showings/showings_presenter.rb
@@ -56,7 +56,7 @@ class ShowingsPresenter
 
   def order_by
     @order_by = available_sortings.include?(@order_by) ? @order_by : available_sortings.first
-    @order_by = available_sortings.first if !geo_filter.used? && sort_by_nearness?
+    @order_by = available_sortings.first if !geo_filter.used?
 
     @order_by
   end
