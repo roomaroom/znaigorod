492f1b86e830e1208479b0e9d93b555ffc1359ff
Dmitriy Baraksanov <bdn@openteam.ru>
2013-03-15 16:42:11 +0700

    def to_s in contact

--- app/models/crm/contact.rb
+++ app/models/crm/contact.rb
@@ -1,6 +1,10 @@
 class Contact < ActiveRecord::Base
   attr_accessible :email, :facebook, :full_name, :mobile_phone, :phone, :post, :skype, :vkontakte
-  validates_presence_of :full_name
+  validates_presence_of :full_name, :post
 
   belongs_to :organization
+
+  def to_s
+    "#{full_name}, #{post}"
+  end
 end
