da4dec048d897e51352cde36a2a4ce8fcaa7d3d0
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-03-18 14:28:14 +0700

    Мжвячни формы

--- app/controllers/crm/organizations_controller.rb
+++ app/controllers/crm/organizations_controller.rb
@@ -4,9 +4,7 @@ class Crm::OrganizationsController < Crm::ApplicationController
   has_scope :page, default: 1
 
   def index
-    params.select! { |key, _| %w[q status user_id suborganizations page].include?(key) }
-
-    @organizations ||= HasSearcher.searcher(:manage_organization, params).
+    @organizations = HasSearcher.searcher(:manage_organization, params[:search]).
       paginate(page: params[:page] || 1, per_page: 10).results
   end
 end
