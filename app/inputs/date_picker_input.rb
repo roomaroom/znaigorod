2833456aad1b36f673f8008682633c5bbb382513
Evgeny Lapin <lev@openteam.ru>
2013-03-19 16:05:48 +0700

    fix custom inputs

--- app/inputs/date_picker_input.rb
+++ app/inputs/date_picker_input.rb
@@ -2,4 +2,12 @@ class DatePickerInput < SimpleForm::Inputs::StringInput
   def input_html_classes
     super.push('date_picker')
   end
+
+  def input
+    @builder.text_field(attribute_name, input_html_options.merge(date_picker_options(object.send(attribute_name))))
+  end
+
+  def date_picker_options(value = nil)
+    date_picker_options = {:value => value.nil?? nil : I18n.localize(value)}
+  end
 end
--- app/inputs/datetime_picker_input.rb
+++ app/inputs/datetime_picker_input.rb
@@ -2,4 +2,12 @@ class DatetimePickerInput < SimpleForm::Inputs::StringInput
   def input_html_classes
     super.push('datetime_picker')
   end
+
+  def input
+    @builder.text_field(attribute_name, input_html_options.merge(datetime_picker_options(object.send(attribute_name))))
+  end
+
+  def datetime_picker_options(value = nil)
+    datetime_picker_options = {:value => value.nil?? nil : I18n.localize(value)}
+  end
 end
