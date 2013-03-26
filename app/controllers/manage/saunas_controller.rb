e9eb87694b484e6380d0e5837020f14d9a0c2fae
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-03-26 15:00:41 +0700

    Fix manage/saunas_controller

--- app/controllers/manage/saunas_controller.rb
+++ app/controllers/manage/saunas_controller.rb
@@ -5,19 +5,7 @@ class Manage::SaunasController < Manage::ApplicationController
 
   belongs_to :organization, :optional => true
 
-  before_filter :redirect_to_edit, :only => :new, :if => :sauna_exists?
-
   def index
     @collection = Sauna.page(params[:page] || 1).per(10)
-  end
-
-  private
-
-  def redirect_to_edit
-    redirect_to edit_manage_organization_sauna_path(parent) and return
-  end
-
-  def sauna_exists?
-    parent.sauna
   end
 end
