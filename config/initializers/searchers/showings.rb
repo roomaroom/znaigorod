e106f1638504017ae4e26e3c4cbfad6cb3a1d0c3
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-03-28 16:19:03 +0700

    revert deleted searcher

--- config/initializers/searchers/showings.rb
+++ config/initializers/searchers/showings.rb
@@ -1,3 +1,65 @@
+HasSearcher.create_searcher :showing do
+  models :showing
+
+  property :affiche_id
+  property :affiche_category
+  property :starts_on, :modificator => :less_than
+  property :starts_at, :modificator => :greater_than
+  property :starts_at_hour, :modificators => [:greater_than, :less_than]
+
+  property :price, :modificators => [:greater_than, :less_than] do |search|
+    if search_object.price_greater_than || search_object.price_less_than
+      price_gt = [0, search_object.price_greater_than.to_i].max
+      price_lt = search_object.price_less_than.to_i.zero? ? 10_000_000 : search_object.price_less_than.to_i
+
+      search.with(:price_min).greater_than(price_gt)
+      search.with(:price_min).less_than(price_lt)
+
+      search.any_of do
+        all_of do
+          with(:price_min).greater_than(price_gt)
+          with(:price_max).less_than(price_lt)
+        end
+
+        all_of do
+          with(:price_min).less_than(price_gt)
+          with(:price_max).greater_than(price_gt)
+        end
+
+        all_of do
+          with(:price_min).less_than(price_lt)
+          with(:price_max).greater_than(price_lt)
+        end
+      end
+    end
+  end
+
+  property :tags
+  property :affiche_category
+
+  property :starts_on, :modificator => :greater_than do |search|
+    starts_on_gt = search_object.starts_on_greater_than || Date.today
+    ends_at_gt = search_object.starts_on_less_than || 1.week.since.to_date
+
+    search.any_of do
+      with(:starts_on).greater_than(starts_on_gt)
+      with(:ends_at).greater_than(ends_at_gt)
+    end
+  end
+
+  scope :actual do
+    with(:starts_at).greater_than HasSearcher.cacheable_now
+  end
+
+  scope :today do
+    with(:starts_at).less_than DateTime.now.end_of_day
+  end
+
+  scope :faceted do
+    facet(:tags)
+  end
+end
+
 HasSearcher.create_searcher :showings do
   models :showing
 
