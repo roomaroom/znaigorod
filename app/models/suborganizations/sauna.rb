01cfcc2a19c7e9ba4d1834f57c84d43c4358272e
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-03-28 12:19:38 +0700

    fix

--- app/models/suborganizations/sauna.rb
+++ app/models/suborganizations/sauna.rb
@@ -60,6 +60,10 @@ class Sauna < ActiveRecord::Base
 
   include PresentsAsCheckboxes
 
+  presents_as_checkboxes :category,
+    :validates_presence => true,
+    :message => I18n.t('activerecord.errors.messages.at_least_one_value_should_be_checked')
+
   presents_as_checkboxes :feature
   presents_as_checkboxes :offer
 
