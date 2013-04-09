c85c4b7afa7323e3898801052e36b9f8d9de3a33
Dmitry Lihachev <lda@openteam.ru>
2013-04-09 13:02:35 +0700

    Крутая форма добавления 3Д-туров с изображением для организаций

--- app/models/virtual_tour.rb
+++ app/models/virtual_tour.rb
@@ -1,13 +1,19 @@
 class VirtualTour < ActiveRecord::Base
-  attr_accessible :link, :image_attributes
+  attr_accessible :link, :attachment
 
   belongs_to :tourable, :polymorphic => true
 
-  has_one :image, :as => :attacheable, :class_name => 'PaperclipAttachment', :conditions => { :attachment_type => :image }
+  has_one :image, :as => :attacheable, :class_name => 'PaperclipAttachment', :conditions => { :attachment_type => :image }, :dependent => :destroy
 
   accepts_nested_attributes_for :image, :allow_destroy => true
 
   validates_presence_of :link
 
   normalize_attribute :link
+
+  delegate :attachment, :attachment_url, :attachment_url?, :attachment=, :to => :image, :allow_nil => true
+
+  def image
+    super || build_image
+  end
 end
