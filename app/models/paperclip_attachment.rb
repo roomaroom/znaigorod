e911e87cc8a59d400fc6928c0bf60e145f16a542
Dmitry Lihachev <lda@openteam.ru>
2013-04-09 10:41:25 +0700

    VirtualTour has_one image

--- app/models/paperclip_attachment.rb
+++ app/models/paperclip_attachment.rb
@@ -1,4 +1,6 @@
 class PaperclipAttachment < ActiveRecord::Base
   belongs_to :attacheable, :polymorphic => true
   attr_accessible :attachment
+
+  has_attached_file :attachment, :storage => :elvfs, :elvfs_url => Settings['storage.url']
 end
--- app/models/virtual_tour.rb
+++ app/models/virtual_tour.rb
@@ -1,6 +1,13 @@
 class VirtualTour < ActiveRecord::Base
+  attr_accessible :link, :image_attributes
+
   belongs_to :tourable, :polymorphic => true
-  attr_accessible :link
+
+  has_one :image, :as => :attacheable, :class_name => 'PaperclipAttachment', :conditions => { :attachment_type => :image }
+
+  accepts_nested_attributes_for :image, :allow_destroy => true
+
   validates_presence_of :link
+
   normalize_attribute :link
 end
