377d9840cf9671e5812b1f3563df419c9bd7ea4e
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-04-22 16:16:12 +0700

    Annotate models :hankey:

--- app/models/coupons/coupon.rb
+++ app/models/coupons/coupon.rb
@@ -3,3 +3,16 @@ class Coupon < ActiveRecord::Base
 
   has_many :offers, dependent: :destroy
 end
+
+# == Schema Information
+#
+# Table name: coupons
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text
+#  discount    :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
--- app/models/coupons/offer.rb
+++ app/models/coupons/offer.rb
@@ -3,3 +3,17 @@ class Offer < ActiveRecord::Base
 
   belongs_to :coupon
 end
+
+# == Schema Information
+#
+# Table name: offers
+#
+#  id          :integer          not null, primary key
+#  coupon_id   :integer
+#  price       :integer
+#  number      :integer
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
