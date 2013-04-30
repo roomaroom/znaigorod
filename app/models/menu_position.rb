17b2ff82f1b63bcf3ebc36483b6aeedfed726e67
Ivan Puchkov <rake.eclipse@gmail.com>
2013-04-30 11:35:39 +0700

    Manage menu code rf

--- app/models/menu_position.rb
+++ app/models/menu_position.rb
@@ -1,5 +1,7 @@
 class MenuPosition < ActiveRecord::Base
   attr_accessible :count, :description, :position, :price, :title, :cooking_time
 
+  validates_presence_of :title, :price
+
   belongs_to :menu
 end
