1402a8adf3daf6e83f77ba4ab60dcd03103333e1
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-05-15 18:09:33 +0700

    12 tickets at page

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,6 +2,6 @@ class TicketsController < ApplicationController
   has_scope :page, default: 1
 
   def index
-    @ticket_infos = TicketInfo.page(params[:page]).per(10)
+    @ticket_infos = TicketInfo.page(params[:page]).per(12)
   end
 end
