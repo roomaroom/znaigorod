a90350a629a31240d0f0e95efb89d643c6040092
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-05-15 09:36:16 +0700

    Use real robokassa server in production

--- config/initializers/activemerchant.rb
+++ config/initializers/activemerchant.rb
@@ -1,3 +1,3 @@
 ActionView::Base.send(:include, ActiveMerchant::Billing::Integrations::ActionViewHelper)
 
-ActiveMerchant::Billing::Base.integration_mode = :test #:production
+ActiveMerchant::Billing::Base.integration_mode = Rails.env.production? ? :production : :test
