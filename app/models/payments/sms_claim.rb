e83afa9ff874a06ef4ee240b34059d3671af5960
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-05-21 09:48:32 +0700

    Sms messages at claim

--- app/models/payments/sms_claim.rb
+++ app/models/payments/sms_claim.rb
@@ -1,19 +1,21 @@
+# encoding: utf-8
+
 class SmsClaim < ActiveRecord::Base
   attr_accessible :details, :name, :phone
 
-  belongs_to :claimed, :polymorphic => true
+  belongs_to :claimed, polymorphic: true
 
-  validates_presence_of :details, :name
+  validates_presence_of :name
+  validates :phone, presence: true, format: { :with => /\+7-\(\d{3}\)-\d{3}-\d{4}/ }
+  validates :details, presence: true, length: { maximum: 220 }
 
-  validates :phone, presence: true, :format => { :with => /\+7-\(\d{3}\)-\d{3}-\d{4}/ }
-
-  has_one :sms, :as => :smsable
+  has_many :smses, as: :smsable
 
   delegate :sms_claimable?, :to => :claimed
 
   before_validation :check_balance
 
-  after_create :send_sms, :pay
+  after_create :send_sms_to_organization, :send_sms_to_purchaser, :pay
 
   private
 
@@ -21,14 +23,26 @@ class SmsClaim < ActiveRecord::Base
     errors[:base] << I18n.t('activerecord.errors.messages.not_enough_money') unless claimed.enough_balance?
   end
 
-  def send_sms
-    create_sms! :phone => claimed.organization.phone_for_sms, :message => 'find me sms_claim.rb'
+  def send_sms_to_organization
+    smses.create! :phone => claimed.organization.phone_for_sms, :message => message_for_organization
+  end
+
+  def send_sms_to_purchaser
+    smses.create! :phone => phone, :message => 'Ваша заявка принята. Сотрудник заведения свяжется с вами.'
   end
 
   def pay
     organization = claimed.organization
     organization.balance_delta = -Settings['sms_claim.price']
     organization.save :validate => false
+  end
+
+  def normalized_phone
+    phone.gsub(/[-()]/, '')
+  end
+
+  def message_for_organization
+    "Поступила заявка. Имя: #{name}, тел: #{normalized_phone}. #{details}"
   end
 end
 
