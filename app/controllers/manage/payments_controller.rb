b7f6e6997ac9d3871c9faafe8f5fb3d882158e2e
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-05-22 11:53:48 +0700

    Fix after merge

--- app/controllers/manage/payments_controller.rb
+++ app/controllers/manage/payments_controller.rb
@@ -2,6 +2,6 @@ class Manage::PaymentsController < Manage::ApplicationController
   has_scope :page, :default => 1
 
   def index
-    @payments = Payment.page(params[:page]).per(20)
+    @payments = Payment.order('id DESC').page(params[:page]).per(20)
   end
 end
