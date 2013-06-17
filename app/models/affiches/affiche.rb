e57488b39a573856bb0733f49e5b0075fde636d3
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-06-17 15:34:08 +0700

    Set slug only once at publishing

--- app/models/affiches/affiche.rb
+++ app/models/affiches/affiche.rb
@@ -67,7 +67,15 @@ class Affiche < ActiveRecord::Base
   attr_accessor :step, :set_region, :crop_x, :crop_y, :crop_width, :crop_height
   attr_accessible :poster_image, :step, :set_region, :crop_x, :crop_y, :crop_width, :crop_height
 
+  def should_generate_new_friendly_id?
+    false
+  end
+
   state_machine :initial => :draft do
+    before_transition any => :published do |affiche, transition|
+      affiche.slug = affiche.normalize_friendly_id(affiche.title) unless affiche.slug?
+    end
+
     state :draft do
     end
 
