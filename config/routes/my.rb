78a8f2f8a658505c2b460796416346da629b0894
tab <tukhfatullin@gmail.com>
2013-06-17 12:14:56 +0700

    Архивные афиши

--- config/routes/my.rb
+++ config/routes/my.rb
@@ -1,5 +1,7 @@
 Znaigorod::Application.routes.draw do
   namespace :my do
+    get 'affiches/archive'
+
     resources :affiches do
       get 'edit/step/:step' => 'affiches#edit', :defaults => { :step => 'first' }, :on => :member, :as => :edit_step
       get 'available_tags'
