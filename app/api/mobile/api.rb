1d0cc1e8decf45468503f86ead9884b5c6885c1a
Evgeny Lapin <lev@openteam.ru>
2013-07-15 16:52:05 +0700

    mobile api: варианты сортировок

--- app/api/mobile/api.rb
+++ app/api/mobile/api.rb
@@ -6,29 +6,49 @@ module Mobile
     format :json
 
     helpers do
+
+      def base_path
+        "http://znaigorod.ru/mobile/affisha"
+      end
+
+      def api_version
+        DateTime.new(2013, 7, 15, 16, 10, 00, '+7')
+      end
+
       def subcategories(kind)
         [
-          { category: 'Все',  url:  "/mobile/affisha/#{kind}/all"},
-          { category: 'Сегодня',  url:  "/mobile/affisha/#{kind}/today"},
-          { category: 'На неделе',  url:  "/mobile/affisha/#{kind}/week"},
-          { category: 'На выходных',  url:  "/mobile/affisha/#{kind}/weekend"}
+          { category: 'Все',  url:  "#{base_path}/#{kind}/all"},
+          { category: 'Сегодня',  url:  "#{base_path}/#{kind}/today"},
+          { category: 'На неделе',  url:  "#{base_path}/#{kind}/week"},
+          { category: 'На выходных',  url:  "#{base_path}/#{kind}/weekend"}
         ]
       end
     end
 
-    resources :affisha do
+    resource :affisha do
       get '/categories' do
-        categories = [ {category: 'Все мероприятия', url: "/mobile/affisha/all", subcategories: subcategories('all') }]
+        categories = [ {category: 'Все мероприятия', url: "#{base_path}/all", subcategories: subcategories('all') }]
         categories += Affiche.ordered_descendants.map do |affiche_class|
           {
             category: affiche_class.model_name.human,
-            url: "/mobile/affisha/#{affiche_class.name.downcase.pluralize}",
+            url: "#{base_path}/#{affiche_class.name.downcase.pluralize}",
             subcategories: subcategories(affiche_class.name.downcase.pluralize)
           }
         end
         {
-          lastUpdate: DateTime.new(2013, 7, 15, 16, 10, 00, '+7'),
+          lastUpdate: api_version,
           categories: categories
+        }
+      end
+
+      get '/sortings' do
+        {
+          lastUpdate: api_version,
+          methods: [
+            { name: 'По новизне', urlModifier: 'creation' },
+            { name: 'По рейтингу', urlModifier: 'rating' },
+            { name: 'По ближайшему сеансу', urlModifier: 'starts_at' }
+          ]
         }
       end
     end
