7edd81d521c16ed03ac28eb89a9dee335ada98fe
Evgeny Lapin <lev@openteam.ru>
2013-07-18 15:21:41 +0700

    Fix my_afisha files

--- app/controllers/my/gallery_files_controller.rb
+++ app/controllers/my/gallery_files_controller.rb
@@ -2,11 +2,11 @@ class My::GalleryFilesController < My::ApplicationController
   actions :create, :destroy
   custom_actions :collection => :destroy_all
 
-  belongs_to :affiche, :polymorphic => true, :optional => true
+  belongs_to :afisha, :polymorphic => true, :optional => true
 
   def create
-    @affiche = Affiche.find(params[:affiche_id])
-    @gallery_file = @affiche.gallery_files.create(:file => params[:gallery_files][:file].first)
+    @afisha = Afisha.find(params[:afisha_id])
+    @gallery_file = @afisha.gallery_files.create(:file => params[:gallery_files][:file].first)
   end
 
   def destroy
@@ -17,8 +17,8 @@ class My::GalleryFilesController < My::ApplicationController
 
   def destroy_all
     destroy_all! {
-      @affiche.gallery_files.destroy_all
-      redirect_to edit_step_my_affiche_path(@affiche.id, :step => :sixth) and return
+      @afisha.gallery_files.destroy_all
+      redirect_to edit_step_my_afisha_path(@afisha.id, :step => :sixth) and return
     }
   end
 end
