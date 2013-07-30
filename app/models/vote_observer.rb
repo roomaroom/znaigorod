671ec3ab1cf24eeed908e7d9a32c48105d476287
tab <tukhfatullin@gmail.com>
2013-07-30 12:12:44 +0700

    Уведомления: комментарий нравится пользователю

--- app/models/vote_observer.rb
+++ app/models/vote_observer.rb
@@ -5,6 +5,9 @@ class VoteObserver < ActiveRecord::Observer
     if vote.voteable.is_a?(Afisha)
       vote.messages.create(body: "Ваша афиша нравится пользователю: #{vote.user.account}",
                            account: vote.voteable.user.present? ? vote.voteable.user.account : nil)
+    elsif vote.voteable.is_a?(Comment)
+      vote.messages.create(body: "Ваш комментарий нравится пользователю: #{vote.user.account}",
+                           account: vote.voteable.user.account)
     end
   end
 end
