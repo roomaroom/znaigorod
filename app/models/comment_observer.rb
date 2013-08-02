0b8a06db2e908b3bf9156e5527ddb6cc0da573d6
tab <tukhfatullin@gmail.com>
2013-08-02 16:44:04 +0700

    Fix notifications for comments

--- app/models/comment_observer.rb
+++ app/models/comment_observer.rb
@@ -3,7 +3,7 @@
 class CommentObserver < ActiveRecord::Observer
   def after_create(comment)
     if comment.commentable.is_a?(Afisha)
-      comment.messages.create(account: comment.is_root? ? comment.commentable.user.account : comment.parent.user.account,
+      comment.messages.create(account: comment.is_root? ? (comment.commentable.user.present? ? comment.commentable.user.account : nil) : comment.parent.user.account,
                               producer_id: comment.user.account_id,
                               body: comment.body,
                               kind: comment.is_root? ? :new_comment : :reply_on_comment)
