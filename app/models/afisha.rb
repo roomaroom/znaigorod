d4ef1bf2189ea4f59fcdea7096c61fc5751b65a8
Ivan Puchkov <rake.eclipse@gmail.com>
2013-08-09 14:56:47 +0700

    add missing attr_accessible

--- app/models/afisha.rb
+++ app/models/afisha.rb
@@ -13,7 +13,7 @@ class Afisha < ActiveRecord::Base
                   :distribution_starts_on, :distribution_ends_on,
                   :original_title, :trailer_code, :vk_aid, :yandex_fotki_url, :constant,
                   :age_min, :age_max, :state_event, :state, :user_id, :kind, :vk_event_url,
-                  :fb_likes, :odn_likes
+                  :fb_likes, :odn_likes, :vkontakte_likes
 
   belongs_to :user
 
