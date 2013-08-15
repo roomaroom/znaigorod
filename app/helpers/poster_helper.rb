8cf94d620b0e725f5fb563f2c0f9134e199a1f2f
Evgeny Lapin <lev@openteam.ru>
2013-08-15 13:24:15 +0700

    change regex in resized_image_url if region present in image url

--- app/helpers/poster_helper.rb
+++ app/helpers/poster_helper.rb
@@ -4,7 +4,7 @@ module PosterHelper
 
   def resized_image_url(url, width, height, options = { crop: '!', orientation: 'n' })
     if url.match(/\d+\/region\/\d+/)
-      return url.gsub(/(\/region\/(\d+|\/)+)/) { "#{$1}#{width}-#{height}/" }
+      return url.gsub(/(\/region\/(\d+|\/){8})/) { "#{$1}#{width}-#{height}/" }
     end
     if url.match(/\/files\/\d+\/\d+-\d+\//)
       image_url, image_id, image_width, image_height, image_crop, image_filename = url.match(%r{(.*)/files/(\d+)/(?:(\d+)-(\d+)(\!)?/)?(.*)})[1..-1]
