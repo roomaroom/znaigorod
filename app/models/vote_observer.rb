c04b35f9e3125997e165b9b4e63a380a26bbaa16
Ivan Puchkov <rake.eclipse@gmail.com>
2013-08-15 11:08:57 +0700

    fix vote observer

--- app/models/vote_observer.rb
+++ app/models/vote_observer.rb
@@ -2,7 +2,7 @@
 
 class VoteObserver < ActiveRecord::Observer
   def after_create(vote)
-    if vote.voteable.is_a?(Afisha) && vote.voteable.user.present?
+    if vote.voteable.is_a?(Afisha) && vote.voteable.user.present? && vote.user.present?
       vote.messages.create(account: vote.voteable.user.account, producer_id: vote.user.account_id, kind: :user_vote_afisha)
     elsif vote.voteable.is_a?(Comment)
       vote.messages.create(account: vote.voteable.user.account, producer_id: vote.user.account_id, kind: :user_vote_comment)
