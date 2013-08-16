38bb9631e409972768863dd4322aab546a41e262
Dmitriy Baraksanov <bdn@openteam.ru>
2013-08-16 09:40:33 +0700

    обзервер дружды

--- app/models/friend_observer.rb
+++ app/models/friend_observer.rb
@@ -2,6 +2,12 @@
 
 class FriendObserver < ActiveRecord::Observer
   def after_create(friend)
-    friend.messages.create(account: friend.friendable, producer: friend.account, kind: :user_add_friend)
+    Message.delay.create(
+      account: friend.friendable,
+      producer: friend.account,
+      kind: :user_add_friend,
+      messageable: friend)
+    friend.friendable.delay.update_rating
+    friend.account.delay.update_rating
   end
 end
