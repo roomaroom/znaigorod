85229fb2d147996866e66104f220d5c208c8b1c0
Ivan Puchkov <rake.eclipse@gmail.com>
2013-08-22 10:27:00 +0700

    new message to account

--- app/controllers/my/private_messages_controller.rb
+++ app/controllers/my/private_messages_controller.rb
@@ -1,24 +1,21 @@
 class My::PrivateMessagesController < My::ApplicationController
+  load_and_authorize_resource
   actions :create
-  #custom_actions collection: :change_message_status
+
+  def new
+    @private_message = begin_of_association_chain.produced_messages.new(account_id: params[:account_id])
+    render partial: 'my/private_messages/form'
+  end
 
   def create
     create! do |success, failure|
       success.html { redirect_to my_dialog_path(@private_message.account)}
-      failure.html { render partial: 'my/dialogs/form' }
+      failure.html { render partial: 'my/private_messages/form' }
     end
   end
 
-  #def change_message_status
-    #change_message_status! {
-      #@private_message = PrivateMessage.find(params[:private_message_id])
-      #@private_message.change_message_status
-
-      #render partial: 'my/messages/message', locals: { message: @private_message } and return
-    #}
-  #end
-
   protected
+
   def begin_of_association_chain
     current_user.account
   end
