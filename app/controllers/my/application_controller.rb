e9006ba5910ee867ba2bae6918c6fd66cb449b15
Ivan Puchkov <rake.eclipse@gmail.com>
2013-08-30 16:30:08 +0700

    remove unnecessary notification

--- app/controllers/my/application_controller.rb
+++ app/controllers/my/application_controller.rb
@@ -7,10 +7,6 @@ class My::ApplicationController < ApplicationController
 
   check_authorization
 
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to new_my_session_path, :notice => "У вас не хватает прав для выполнения этого действия"
-  end
-
   def namespace
     params[:controller].split('/').first
   end
