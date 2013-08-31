d9b802b7337bf01e51e1d2bf6969e5df32732a15
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-08-31 20:39:32 +0700

    RD to new_my_session_path

--- app/controllers/my/application_controller.rb
+++ app/controllers/my/application_controller.rb
@@ -4,6 +4,7 @@ class My::ApplicationController < ApplicationController
   inherit_resources
 
   helper_method :namespace
+  before_filter :check_current_user
 
   check_authorization
 
@@ -13,5 +14,10 @@ class My::ApplicationController < ApplicationController
 
   def current_ability
     @current_ability ||= Ability.new(current_user, namespace)
+  end
+
+  private
+  def check_current_user
+    redirect_to new_my_session_path unless current_user
   end
 end
