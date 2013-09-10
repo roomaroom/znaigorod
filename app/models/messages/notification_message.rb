ee10ad48d4953561a4e02e309c37ad0055538a50
Ivan Puchkov <rake.eclipse@gmail.com>
2013-09-10 16:27:46 +0700

    add missed attributes to enum

--- app/models/messages/notification_message.rb
+++ app/models/messages/notification_message.rb
@@ -7,7 +7,7 @@ class NotificationMessage < Message
     in: [:new_comment, :reply_on_comment, :afisha_published,
          :afisha_returned, :user_vote_afisha, :user_vote_comment,
          :user_visit_afisha, :user_add_friend,
-         :auction_bet, :auction_bet_cancel, :auction_bet_approve, :auction_bet_pay],
+         :auction_bet, :auction_bet_cancel, :auction_bet_approve, :auction_bet_pay, :agreed_invite, :disagreed_invite],
     predicates: true
 
 
