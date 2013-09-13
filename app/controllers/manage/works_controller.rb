9daf003eec01d56d0282bf51930af272f3cfa512
tab <tukhfatullin@gmail.com>
2013-09-13 17:50:10 +0700

    fix

--- app/controllers/manage/works_controller.rb
+++ app/controllers/manage/works_controller.rb
@@ -2,4 +2,12 @@ class Manage::WorksController < Manage::ApplicationController
   actions :all, :except => [:index, :show]
 
   belongs_to :contest
+
+  def create
+    create! { manage_contest_path(@contest) }
+  end
+
+  def update
+    update! { manage_contest_path(@contest) }
+  end
 end
