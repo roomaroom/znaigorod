9d01095cd8a6e275aa61da1703c3b61dd30ca863
tab <tukhfatullin@gmail.com>
2013-09-17 12:34:10 +0700

    Fix poster url for popular api

--- app/api/affiches/api.rb
+++ app/api/affiches/api.rb
@@ -9,7 +9,7 @@ module Affiches
       expose (:place)       { |model, options| AfishaDecorator.new(model).place.gsub(/\u00AD+/,'') }
       expose (:kind)        { |model, options| AfishaDecorator.new(model).kind.map(&:text).join(', ').gsub(/\u00AD+/,'') }
       expose (:url)         { |model, options| "#{Settings['app.url']}/afisha/#{model.slug}" }
-      expose (:poster_url)  { |model, options| AfishaDecorator.new(model).resized_image_url(model.poster_image_url, 290, 390) }
+      expose (:poster_url)  { |model, options| AfishaDecorator.new(model).resized_image_url(model.poster_url, 290, 390) }
     end
   end
 
@@ -19,7 +19,7 @@ module Affiches
 
     resource :popular do
       get do
-        present HasSearcher.searcher(:showings).order_by_rating.actual.results.map(&:afisha).first(6), :with => Entities::Afisha
+        present HasSearcher.searcher(:showings).order_by_rating.actual.results.map(&:afisha).uniq.first(6), :with => Entities::Afisha
       end
     end
   end
