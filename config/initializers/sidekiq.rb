a2703241cb99d1d5a69b584345d99a152589d6d6
Ivan Puchkov <rake.eclipse@gmail.com>
2013-09-18 16:27:20 +0700

    some fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => Settings[:redis][:url], :namespace => 'znaigorod' }
+  config.redis = { :url => Settings[:redis][:url], :namespace => 'znaigorod-dev' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => Settings[:redis][:url], :namespace => 'znaigorod' }
+  config.redis = { :url => Settings[:redis][:url], :namespace => 'znaigorod-dev' }
 end
