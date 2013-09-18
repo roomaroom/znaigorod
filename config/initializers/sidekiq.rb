62710e3f5d7276f9cd7a95723cc7fb1bfc241430
Ivan Puchkov <rake.eclipse@gmail.com>
2013-09-18 16:39:56 +0700

    revert changes in sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => Settings[:redis][:url], :namespace => 'znaigorod-dev' }
+  config.redis = { :url => Settings[:redis][:url], :namespace => 'znaigorod' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => Settings[:redis][:url], :namespace => 'znaigorod-dev' }
+  config.redis = { :url => Settings[:redis][:url], :namespace => 'znaigorod' }
 end
