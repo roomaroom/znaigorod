083e719518eede70850a1282d5b66392c1b6e79a
Ivan Puchkov <rake.eclipse@gmail.com>
2013-09-26 15:37:36 +0700

    fix read only

--- app/models/afisha.rb
+++ app/models/afisha.rb
@@ -71,7 +71,7 @@ class Afisha < ActiveRecord::Base
   scope :draft,                 -> { with_state(:draft) }
   scope :published,             -> { with_state(:published) }
   scope :pending,               -> { with_state(:pending) }
-  scope :actual,                -> { joins(:showings).where('showings.starts_at >= ? OR (showings.ends_at is not null AND showings.ends_at > ?)', DateTime.now.beginning_of_day, Time.zone.now).uniq }
+  scope :actual,                -> { includes(:showings).where('showings.starts_at >= ? OR (showings.ends_at is not null AND showings.ends_at > ?)', DateTime.now.beginning_of_day, Time.zone.now).uniq }
   scope :archive,               -> { joins(:showings).where('showings.starts_at < ? OR (showings.ends_at is not null AND showings.ends_at < ?)', DateTime.now.beginning_of_day, Time.zone.now).uniq }
 
   friendly_id :title, use: :slugged
