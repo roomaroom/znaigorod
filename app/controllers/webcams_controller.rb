bea3ed781acc3269fff87324596b2a0e274f081e
Evgeny Lapin <lev@openteam.ru>
2013-10-02 18:01:24 +0700

    fix webcam view, controller

--- app/controllers/webcams_controller.rb
+++ app/controllers/webcams_controller.rb
@@ -7,6 +7,10 @@ class WebcamsController < ApplicationController
   has_scope :published, :type => :boolean, :default => true
   has_scope :ordered, :default => 'title'
 
+  def index
+    @webcams = Webcam.ordered(:title).published
+  end
+
   def show
     show! { render :layout => 'webcam' and return }
   end
