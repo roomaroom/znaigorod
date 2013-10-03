074fb7ed8c22d4997945c4b9d81538906e147077
Evgeny Lapin <lev@openteam.ru>
2013-10-03 15:03:55 +0700

    fix error "encode! for nil:NilClass"

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -190,7 +190,7 @@ class Organization < ActiveRecord::Base
 
   def create_page_visit(session, user_agent, user)
     page_visit = self.page_visits.new
-    page_visit.user_agent = user_agent.encode!('UTF-8', :undef => :replace, :invalid => :replace, :replace => '')
+    page_visit.user_agent = user_agent.to_s.encode!('UTF-8', :undef => :replace, :invalid => :replace, :replace => '')
     page_visit.user = user
     page_visit.save
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -69,7 +69,7 @@ class Post < ActiveRecord::Base
 
   def create_page_visit(session, user_agent, user)
     page_visit = self.page_visits.new
-    page_visit.user_agent = user_agent.encode!('UTF-8', :undef => :replace, :invalid => :replace, :replace => '')
+    page_visit.user_agent = user_agent.to_s.encode!('UTF-8', :undef => :replace, :invalid => :replace, :replace => '')
     page_visit.user = user
     page_visit.save
   end
