fe3fef35bf8c869a31e423e2380aa9f0e65565c6
Evgeny Lapin <lev@openteam.ru>
2013-10-09 15:26:46 +0700

    add invitations_for_tipsy

---
+++ app/helpers/account_helper.rb
@@ -0,0 +1,44 @@
+# encoding: utf-8
+
+module AccountHelper
+
+  def invitations_for_tipsy(invitations)
+    content = ''
+    if invitations.inviter.without_invited.any?
+      invitations.inviter.without_invited.each do |invitation|
+        content += '<p>'
+        content += 'Приглашает'
+        content += ' '
+        content += t("enumerize.invitation.gender.custom.#{invitation.kind}.#{invitation.gender}")
+        content += ' '
+        if invitation.inviteable
+          content += Preposition.new(invitation.inviteable).value
+          content += ' '
+          content += invitation.inviteable.title
+        else
+          content += invitation.category
+        end
+        content += '</p>'
+      end
+    end
+    if invitations.invited.without_invited.any?
+      invitations.invited.without_invited.each do |invitation|
+        content += '<p>'
+        content += 'Ждет приглашения'
+        content += ' '
+        content += t("enumerize.invitation.gender.custom.#{invitation.kind}.#{invitation.gender}")
+        content += ' '
+        if invitation.inviteable
+          content += Preposition.new(invitation.inviteable).value
+          content += ' '
+          content += invitation.inviteable.title
+        else
+          content += invitation.category
+        end
+        content += '</p>'
+      end
+    end
+    content
+  end
+
+end
