3d0bd239d3a277954fc05b2ff644741e819c51cc
Evgeny Lapin <lev@openteam.ru>
2013-10-09 11:18:08 +0700

    cache fragment webcams index

--- app/controllers/manage/webcams_controller.rb
+++ app/controllers/manage/webcams_controller.rb
@@ -4,4 +4,12 @@ class Manage::WebcamsController < Manage::ApplicationController
 
   has_scope :ordered, :type => :boolean, :default => true
 
+  after_filter :reset_cache, :only => [:create, :update]
+
+  protected
+
+  def reset_cache
+    expire_fragment('all_available_webcams') if @webcam.errors.empty?
+  end
+
 end
