7f278f1658eea99250da3e38e8eccb414cfcc651
tab <tukhfatullin@gmail.com>
2013-10-10 11:57:00 +0700

    Paginate coupons in manage

--- app/controllers/manage/discounts_controller.rb
+++ app/controllers/manage/discounts_controller.rb
@@ -1,3 +1,9 @@
 class Manage::DiscountsController < Manage::ApplicationController
   actions :all
+
+  private
+
+  def collection
+    @collection = Discount.page(params[:page] || 1).per(10)
+  end
 end
