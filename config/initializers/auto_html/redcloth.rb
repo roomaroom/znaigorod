11d6c7dee3ca456ae1d3dca6376dfee2a67f2e7e
Evgeny Lapin <lev@openteam.ru>
2013-10-11 15:44:38 +0700

    fix auto_html redcloth filter

--- config/initializers/auto_html/redcloth.rb
+++ config/initializers/auto_html/redcloth.rb
@@ -7,7 +7,7 @@ AutoHtml.add_filter(:redcloth).with({}) do |text, options|
   attributes = Array(options).reject { |k,v| v.nil? }.map { |k, v| %{#{k}="#{REXML::Text::normalize(v)}"} }.join(' ')
   result = RedCloth.new(text).to_html
   result = Rinku.auto_link(result, :all, attributes)
-  result = Gilenson.new(result).to_html.html_safe
+  result = Gilenson.new(result.gsub('&#8220;', '"').gsub('&#8221;', '"')).to_html.html_safe
   result.gsub!(/<a/,'<a target="_blank"') if options and options[:target] and options[:target].to_sym == :_blank
   result.gsub!(/<a/,'<a rel="nofollow"') if options and options[:rel] and options[:rel].to_sym == :nofollow
 
