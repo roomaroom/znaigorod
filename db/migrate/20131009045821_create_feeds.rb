716ab48f333ce4b8308d9aae8c60969eba46f2d8
Nimdis <tsaazi@gmail.com>
2013-10-14 15:52:33 +0700

    Fixed migration, finished invitations

--- db/migrate/20131009045821_create_feeds.rb
+++ db/migrate/20131009045821_create_feeds.rb
@@ -26,7 +26,7 @@ class CreateFeeds < ActiveRecord::Migration
     end
 
     puts 'Invitation'
-    items = Invitation.where('account_id IS NOT NULL')
+    items = Invitation.where('account_id IS NOT NULL AND invited_id IS NULL')
     bar = ProgressBar.new(items.count)
     items.each do |item|
       Feed.create(
