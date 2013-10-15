26a7ddeb3a03faf35f910fed89192882ab4f1b59
Nimdis <tsaazi@gmail.com>
2013-10-15 16:37:30 +0700

    last visit for accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -109,7 +109,7 @@ class Account < ActiveRecord::Base
   end
 
   def age
-    now = Time.now.utc.to_date
+    now = Time.zone.now.to_date
     now.year - self.birthday.year - ((now.month > self.birthday.month || (now.month == self.birthday.month && now.day >= self.birthday.day)) ? 0 : 1)
   end
 
