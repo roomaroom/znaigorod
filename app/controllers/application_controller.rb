4623dfcc30b3d3abf985209c853af275c1daa4eb
Evgeny Lapin <lev@openteam.ru>
2013-10-17 15:38:56 +0700

    debug in development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   private
 
   def detect_robots_in_development
-    puts
+    puts "\n\n"
     puts "DEBUG ---> #{request.user_agent.to_s}"
     render :nothing => true, status: :forbidden and return if request.user_agent.to_s.match(/\(.*https?:\/\/.*\)/)
   end
