5039eef1e57e275454ad6cf2de809e926f7e38ea
Ivan Puchkov <rake.eclipse@gmail.com>
2013-10-17 14:00:33 +0700

    account meta info for vk sharing

--- app/decorators/account_decorator.rb
+++ app/decorators/account_decorator.rb
@@ -18,4 +18,17 @@ class AccountDecorator < ApplicationDecorator
   def buddies
     friends.approved.map(&:friendable).first(5)
   end
+
+  def tags_for_vk
+    res = ""
+    vk_image = h.image_path('vk_logotype.png')
+    res << "<meta property='og:description' content='Ты можешь найти компанию для любого мероприятия и времяпрепровождения. Посмотри, сколько людей приглашают сходить куда-нибудь, а сколько ждут приглашения!'/>\n"
+    res << "<meta property='og:site_name' content='#{I18n.t('meta.default.title')}' />\n"
+    res << "<meta property='og:title' content='Знакомтсва на ЗнайГород' />\n"
+    res << "<meta property='og:url' content='#{show_url}' />\n"
+    res << "<meta property='og:image' content='#{vk_image}' />\n"
+    res << "<meta name='image' content='#{vk_image}' />\n"
+    res << "<link rel='image_src' href='#{vk_image}' />\n"
+    res.html_safe
+  end
 end
