f879f29878ed11606edea0d14a37e3642526df4e
Nimdis <tsaazi@gmail.com>
2013-10-25 14:54:01 +0700

    anti mearge hack =(

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,8 +1,22 @@
 class Member < ActiveRecord::Base
-  belongs_to :memberable
+  belongs_to :memberable, :polymorphic => true
   belongs_to :account
 
+  has_one :feed, :as => :feedable, :dependent => :destroy
+
   validates_uniqueness_of :account_id, :scope => [:memberable_id, :memberable_type]
+
+  after_create :create_feed
+
+  def create_feed
+    Feed.create(
+      :feedable => self,
+      :account => self.account,
+      :created_at => self.created_at,
+      :updated_at => self.updated_at
+    )
+  end
+
 end
 
 # == Schema Information
