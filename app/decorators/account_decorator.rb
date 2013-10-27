ccb25ea270683c06d0a6a10e5582ec2e8776f4d7
Dmitriy Baraksanov <bdn@openteam.ru>
2013-10-27 10:23:48 +0700

    Знакомтсва -> Знакомства

--- app/decorators/account_decorator.rb
+++ app/decorators/account_decorator.rb
@@ -24,7 +24,7 @@ class AccountDecorator < ApplicationDecorator
     vk_image = h.image_direct_url('public/vk_logotype.png')
     res << "<meta property='og:description' content='Ты можешь найти компанию для любого мероприятия и времяпрепровождения. Посмотри, сколько людей приглашают сходить куда-нибудь, а сколько ждут приглашения!'/>\n"
     res << "<meta property='og:site_name' content='#{I18n.t('meta.default.title')}' />\n"
-    res << "<meta property='og:title' content='Знакомтсва на ЗнайГород' />\n"
+    res << "<meta property='og:title' content='Знакомства на ЗнайГород' />\n"
     res << "<meta property='og:url' content='#{show_url}' />\n"
     res << "<meta property='og:image' content='#{vk_image}' />\n"
     res << "<meta name='image' content='#{vk_image}' />\n"
