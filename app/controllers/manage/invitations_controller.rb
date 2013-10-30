6a8ad6a1dd74398951ce7899cd750d99f6babdaf
Ivan Puchkov <rake.eclipse@gmail.com>
2013-10-30 14:50:30 +0700

    values for date fields

--- app/controllers/manage/invitations_controller.rb
+++ app/controllers/manage/invitations_controller.rb
@@ -1,13 +1,19 @@
 class Manage::InvitationsController < Manage::ApplicationController
   def index
-    if params[:search]
-      starts_at = Time.zone.parse(params[:search]['starts_at'])
-      ends_at = params[:search]['ends_at'].present? ? Time.zone.parse(params[:search]['ends_at']) : Time.zone.today
-
-      @invitations = Invitation.with_invited.starts_at(starts_at.strftime('%Y-%m-%d')).ends_at(ends_at.strftime('%Y-%m-%d'))
+    invitations = Invitation.with_invited
+    if params[:search] && params[:search]['starts_at'].present?
+      @starts_at = Time.zone.parse(params[:search]['starts_at'])
     else
-      @invitations = Invitation.with_invited
+      @starts_at = invitations.first.created_at
     end
+
+    if params[:search] && params[:search]['ends_at'].present?
+      @ends_at = Time.zone.parse(params[:search]['ends_at'])
+    else
+      @ends_at = Time.zone.today
+    end
+
+    @invitations = invitations.starts_at(@starts_at.strftime('%Y-%m-%d')).ends_at(@ends_at.strftime('%Y-%m-%d'))
   end
 end
 
