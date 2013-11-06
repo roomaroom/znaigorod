6a7a7c579f30d72be92ca6bb81d77c193a82db8d
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-11-06 11:53:25 +0700

    use right method

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -13,7 +13,7 @@ class DiscountsController < ApplicationController
   def show
     show! {
       @discount = DiscountDecorator.new(@discount)
-      @presenter = DiscountsPresenter.new(params.merge(:kind => @discount.kind.map(&:value).first, :type => @discount.model.class.name.underscore))
+      @presenter = DiscountsPresenter.new(params.merge(:kind => @discount.kind.map(&:value).first, :type => @discount.model.type_for_solr))
       @discount.delay(:queue => 'critical').create_page_visit(request.session_options[:id], request.user_agent, current_user)
       @members = @discount.members.page(1).per(3)
     }
