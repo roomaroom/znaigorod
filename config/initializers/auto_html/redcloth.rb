95f713f2313a9264f8275c51c2cd06ffa63441a6
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-11-07 14:50:04 +0700

    replace attributes for external link

--- config/initializers/auto_html/redcloth.rb
+++ config/initializers/auto_html/redcloth.rb
@@ -3,13 +3,25 @@ require 'redcloth'
 require 'rexml/document'
 require 'rinku'
 
+def add_attributes_to_links(html)
+  fragment = Nokogiri::HTML.fragment(html)
+
+  fragment.css('a').each do |a|
+    unless a['href'] =~ /znaigorod.ru/
+      a['target'] = '_blank'
+      a['rel'] = 'nofollow'
+    end
+  end
+
+  fragment.to_html
+end
+
 AutoHtml.add_filter(:redcloth).with({}) do |text, options|
   attributes = Array(options).reject { |k,v| v.nil? }.map { |k, v| %{#{k}="#{REXML::Text::normalize(v)}"} }.join(' ')
   result = RedCloth.new(text).to_html
   result = Rinku.auto_link(result, :all, attributes)
+  result = add_attributes_to_links(result)
   result = Gilenson.new(result.gsub('&#8220;', '"').gsub('&#8221;', '"')).to_html.html_safe
-  result.gsub!(/<a/,'<a target="_blank"') if options and options[:target] and options[:target].to_sym == :_blank
-  result.gsub!(/<a/,'<a rel="nofollow"') if options and options[:rel] and options[:rel].to_sym == :nofollow
 
   result
 end
