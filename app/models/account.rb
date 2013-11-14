48de704fdc4c4ab15181b101096ea4ed6cd1726b
Nimdis <tsaazi@gmail.com>
2013-11-14 16:33:32 +0700

    mass send to discounts authors

--- app/models/account.rb
+++ app/models/account.rb
@@ -117,6 +117,17 @@ class Account < ActiveRecord::Base
     end
   end
 
+  def with_avatar?
+    !avatar.url.match(/files\/[44241|44240|44242]/)
+  end
+
+  def self.send_deiscount_statistics
+    accounts = Account.all
+    accounts.each do |account|
+      Discount.send_statistics account
+    end
+  end
+
   def resolve_default_avatar_url
     return "#{Settings['storage.url']}/files/44240/200-200/default_female_avatar.png"     if gender.female?
     return "#{Settings['storage.url']}/files/44241/200-200/default_male_avatar.png"       if gender.male?
