9c571613f133c361148045eeb7d00a1468f73171
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-11-15 11:48:22 +0700

    rbkmoney for cerfiticates created via my

--- app/controllers/my/certificates_controller.rb
+++ app/controllers/my/certificates_controller.rb
@@ -27,7 +27,7 @@ class My:: CertificatesController < My::ApplicationController
 
     def build_resource
       old_build_resource.tap do |object|
-        object.payment_system = :robokassa
+        object.payment_system = :rbkmoney
         object.account = current_user.account
       end
     end
