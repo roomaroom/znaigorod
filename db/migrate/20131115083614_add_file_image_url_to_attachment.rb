e03dce6b9b51c3ce6405b9f167a9929e5d45aa08
Nimdis <tsaazi@gmail.com>
2013-11-15 16:48:21 +0700

    migration for gallery done

--- db/migrate/20131115083614_add_file_image_url_to_attachment.rb
+++ db/migrate/20131115083614_add_file_image_url_to_attachment.rb
@@ -1,5 +1,25 @@
+require 'progress_bar'
+
 class AddFileImageUrlToAttachment < ActiveRecord::Migration
-  def change
+  def up
     add_column :attachments, :file_image_url, :string
+
+    puts 'Setting new gallery fields. Please wait...'
+
+    bar = ProgressBar.new(Account.count)
+
+    Account.all.each do |account|
+      if account.gallery_images.any?
+        account.gallery_images.each do |image|
+          image.file_image_url = image.file_url
+          image.save!
+        end
+      end
+      bar.increment!
+    end
+  end
+
+  def down
+    remove_column :attachments, :file_image_url
   end
 end
