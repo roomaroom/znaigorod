b8aaec44fba9a1a43ea2996a841b836fadde4422
Nimdis <tsaazi@gmail.com>
2013-11-18 16:40:02 +0700

    commit for merge

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -3,27 +3,27 @@ class NoticeMailer < ActionMailer::Base
 
   def personal_invitation invite
     @invite = invite
-    mail(:to => invite.invited.email, :subject => 'invite hello')
+    #mail(:to => invite.invited.email, :subject => 'invite hello')
   end
 
   def private_message message
     @message = message
-    mail(:to => message.account.email, :subject => 'messagehello')
+    #mail(:to => message.account.email, :subject => 'messagehello')
   end
 
   def comment_to_afisha comment
     @comment = comment
-    mail(:to => comment.commentable.user.account.email, :subject => 'afisha comment hello')
+    #mail(:to => comment.commentable.user.account.email, :subject => 'afisha comment hello')
   end
 
   def comment_reply comment
     @comment = comment
-    mail(:to => comment.parent.user.account.email, :subject => 'comment reply hello')
+    #mail(:to => comment.parent.user.account.email, :subject => 'comment reply hello')
   end
 
   def comment_like vote
     @vote = vote
-    mail(:to => vote.voteable.user.account.email, :subject => 'comment like hello')
+    #mail(:to => vote.voteable.user.account.email, :subject => 'comment like hello')
   end
 
   def afisha_statistics afishas, account
