489cf15455203ce9e3e99d15b019d6485d81729a
Nimdis <tsaazi@gmail.com>
2013-11-19 16:57:12 +0700

    annotations for account_settings

--- app/models/account_settings.rb
+++ app/models/account_settings.rb
@@ -3,3 +3,22 @@ class AccountSettings < ActiveRecord::Base
   attr_accessible :personal_invites, :personal_messages, :comments_to_afishas, :comments_to_discounts, :comments_answers, :comments_likes, :afishas_statistics, :discounts_statistics, :account, :created_at, :updated_at
 
 end
+
+# == Schema Information
+#
+# Table name: account_settings
+#
+#  id                    :integer          not null, primary key
+#  account_id            :integer
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#  personal_invites      :boolean          default(TRUE)
+#  personal_messages     :boolean          default(TRUE)
+#  comments_to_afishas   :boolean          default(TRUE)
+#  comments_to_discounts :boolean          default(TRUE)
+#  comments_answers      :boolean          default(TRUE)
+#  comments_likes        :boolean          default(TRUE)
+#  afishas_statistics    :boolean          default(TRUE)
+#  discounts_statistics  :boolean          default(TRUE)
+#
+
