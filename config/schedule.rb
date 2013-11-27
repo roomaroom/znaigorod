7f171784dfc4f1479938f92af73056e584ca3b34
Nimdis <tsaazi@gmail.com>
2013-11-27 14:40:55 +0700

    whenever configured for emails

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,6 +8,10 @@ else
   set :job_template, "/bin/bash -l -i -c ':job' 1>#{dir.log('schedule.log')} 2>#{dir.log('schedule-errors.log')}"
 end
 
+every :day, :at => '6:00 am' do
+  rake 'send_statistics'
+end
+
 every :day, :at => '7:15 am' do
   rake 'sync:fakel'
 end
