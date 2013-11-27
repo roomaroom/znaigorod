8e879857200203d367504d79d992d7583ba23253
Nimdis <tsaazi@gmail.com>
2013-11-27 11:34:32 +0700

    subjects of emails

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -6,51 +6,51 @@ class NoticeMailer < ActionMailer::Base
   def personal_invitation invite
     @invite = invite
     @type = "personal_invitation"
-    mail(:to => 'tsaazi@gmail.com', :subject => '', :layout => "layout").deliver!
+    mail(:to => 'tsaazi@gmail.com', :subject => t("notice_mailer.new_invitation"), :layout => "layout").deliver!
   end
 
   def private_message message
     @message = message
     @type = "private_message"
-    mail(:to => message.account.email, :subject => 'messagehello').deliver!
+    mail(:to => message.account.email, :subject => t("notice_mailer.new_private_message")).deliver!
   end
 
   def comment_to_afisha comment
     @comment = comment
     @type = "comment_to_afisha"
-    mail(:to => comment.commentable.user.account.email, :subject => 'afisha comment hello')
+    mail(:to => comment.commentable.user.account.email, :subject => t("notice_mailer.new_comment_to_afisha"))
   end
 
   def comment_to_discount comment
     @comment = comment
     @type = "comment_to_discount"
-    mail(:to => comment.commentable.account.email, :subject => 'discount comment hello')
+    mail(:to => comment.commentable.account.email, :subject => t("notice_mailer.new_comment_to_discount"))
   end
 
   def comment_reply comment
     @comment = comment
     @type = "comment_reply"
-    mail(:to => comment.parent.user.account.email, :subject => 'comment reply hello')
+    mail(:to => comment.parent.user.account.email, :subject => t("notice_mailer.new_comment_reply"))
   end
 
   def comment_like vote
     @vote = vote
     @type = "comment_like"
-    mail(:to => vote.voteable.user.account.email, :subject => 'comment like hello')
+    mail(:to => vote.voteable.user.account.email, :subject => t("notice_mailer.new_comment_like"))
   end
 
   def afisha_statistics afishas, account
     @account = account
     @afishas = afishas
     @type = "afisha_statistics"
-    mail(:to => account.email, :subject => 'afisha stat hello').deliver!
+    mail(:to => account.email, :subject => t("notice_mailer.afisha_statistics")).deliver!
   end
 
   def discount_statistics discounts, email
     @discounts = discounts
     @account = account
     @type = "discount_statistics"
-    mail(:to => account.email, :subject => 'discount stat hello').deliver!
+    mail(:to => account.email, :subject => t("notice_mailer.discount_statistics")).deliver!
   end
 
   def send_message
