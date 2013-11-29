1ebe20a223771fe5aa3b1576d533c9fdd7c2c8d0
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-11-29 11:32:03 +0700

    create right places

--- app/models/discounts/offered_discount.rb
+++ app/models/discounts/offered_discount.rb
@@ -12,17 +12,25 @@ class OfferedDiscount < Discount
 
   after_create :create_places, :if => :afisha_id?
 
+  private
+
   def handle_before_validation
-    if afisha_id?
-      places.clear
+    places.clear if new_record? && afisha_id?
+  end
+
+  def create_places_from_organizations_addresses
+    afisha.organizations.each { |organization| places.create! :organization_id => organization.id }
+  end
+
+  def create_places_from_showings
+    afisha.showings.pluck(:place).each do |place|
+      geo_info = YampGeocoder.new.geo_info_for(place)
+      places.create! :address => place, :longitude => geo_info.longitude, :latitude => geo_info.latitude
     end
   end
 
   def create_places
-    if afisha.organizations.any?
-      afisha.organizations.each { |o| places.create! :organization_id => o.id }
-    else
-
-    end
+    afisha.organizations.any? ? create_places_from_organizations_addresses
+      : create_places_from_showings
   end
 end
