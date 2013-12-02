d488d21e46ef8e5d5bb60be91adf8c23a53de853
Nimdis <tsaazi@gmail.com>
2013-12-02 16:44:24 +0700

    saunas discounts presenter

---
+++ app/presenters/saunas_discounts_presenter.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+class SaunasDiscountsPresenter
+  def initialize(args)
+    @count = 10
+  end
+
+  def ids
+    Organization.search { with :suborganizations, 'sauna'; paginate :page => 1, :per_page => 1000 }.hits.map(&:primary_key)
+  end
+
+  def collection
+    discount_coupons = DiscountsPresenter.new({:organization_id => ids, :type => ["sauna"], :order_by => "random", :type => 'certificate'}).collection
+    discount_offered = DiscountsPresenter.new({:organization_id => ids, :type => ["sauna"], :order_by => "random", :type => 'offered_discount'}).collection
+    @discount_collection = discount_offered.zip(discount_coupons)
+                                           .flatten.compact.first(@count)
+  end
+
+end
