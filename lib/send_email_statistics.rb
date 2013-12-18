a25ac6de524c5352ccaa0bbb5b86e9a401939744
Nimdis <tsaazi@gmail.com>
2013-12-18 16:47:05 +0700

    a lot of changes; events and discounts alpha version done

--- lib/send_email_statistics.rb
+++ lib/send_email_statistics.rb
@@ -143,22 +143,71 @@ class SendEmailStatistics
     comments
   end
 
+  #============================================================
+  @count = 4
+
   def self.actual_events(period)
-    Afisha.actual.where("afisha.created_at > '#{DateTime.now - period}'").sort_by{|a| -a[:total_rating]}
+    afisha = Afisha.actual.where("afisha.created_at > '#{DateTime.now - period}'").sort_by{|a| -a[:total_rating]}
+    puts afisha.count
+    puts @count.inspect
+    if afisha.count < @count
+      sorted_afisha = Afisha.actual.sort_by{|a| -a[:total_rating]}
+      (0..(@count - afisha.count - 1)).each do |i|
+        afisha.push sorted_afisha[i]
+      end
+    end
+    afisha.first(@count)
   end
 
   def self.new_discounts(period)
-    @coupons = DiscountsPresenter.new(:type => 'coupon', :order_by => 'creation').collection
-    @certificates = DiscountsPresenter.new(:type => 'certificate', :order_by => 'creation').collection
-    [@coupons, @certificates].flatten.sort_by{ |e| e.created_at }.select{|x| x.created_at > (DateTime.now - period)}
+    @berloga = DiscountsPresenter.new(:type => 'coupon', :organization_id => 183, :order_by => 'rating').collection
+    @certificates = DiscountsPresenter.new(:type => 'certificate', :order_by => 'rating').collection
+    @offered_discount = DiscountsPresenter.new(:type => 'offered_discount', :order_by => 'rating').collection
+    discounts = [@berloga, @certificates, @offered_discount].flatten
+                                                .compact
+                                                .sort_by{ |e| e.created_at }
+                                                .select{|x| x.created_at > (DateTime.now - period)}
+    if discounts.select{|x| x.created_at > (DateTime.now - period)}.count < @count
+      sorted_discounts = [@berloga, @certificates, @offered_discount].flatten
+                                                .compact
+                                                .sort_by{ |e| -e[:total_rating] }
+
+      (0..(@count - discounts.count - 1)).each do |i|
+        discounts.push sorted_discounts[i]
+      end
+    else
+      discounts = discounts.select{|x| x.created_at > (DateTime.now - period)}
+    end
+    discounts
   end
 
   def self.dating(gender, period)
     if %w[female male].include?(gender)
-      Account.where("gender = '#{gender}' and created_at > '#{DateTime.now - period}'")
+      accounts = []
+      prepared_accounts = Account.where("gender = '#{gender}' and created_at > '#{DateTime.now - period}'").order("rating DESC")
+      prepared_accounts
+      prepared_accounts.each do |a|
+        if a.rating.present? && a.with_avatar? && a.invitations.with_categories.any?
+          accounts.push a
+        end
+      end
+      accounts.any? ? accounts : nil
     else
       nil
     end
+  end
+
+  def self.new_organizations(period)
+    organizations = OrganizationsCatalogPresenter.new(:per_page => 1000, :sms_claimable => true, :only_clients => true)
+                                                 .collection
+                                                 .sort_by { |e| e[:created_at] }
+                                                 .select{|x| x.created_at > (DateTime.now - 1.week)}
+    unless organizations.any?
+      organizations.reverse.first(4)
+    else
+      organizations.sort_by {|e| e[:total_rating]}.first(4)
+    end
+
   end
 
 end
