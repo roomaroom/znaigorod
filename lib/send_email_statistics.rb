a1f9cf77837466d9ab7276a1ba014bac1e053cf1
Nimdis <tsaazi@gmail.com>
2013-12-19 14:23:14 +0700

    statistics done

--- lib/send_email_statistics.rb
+++ lib/send_email_statistics.rb
@@ -5,47 +5,66 @@ class SendEmailStatistics
   def self.send_discount_statistics
     accounts = Account.where("email is not null")
     managers = Role.all.map(&:user).map(&:account).uniq
+    counter = 0
     (accounts - managers).each do |account|
-      if account.account_settings.discounts_statistics
-        send_discounts account
+      begin
+        if account.account_settings.discounts_statistics
+          if send_discounts(account)
+            counter = counter + 1
+          end
+        end
+      rescue
+        next
       end
     end
+    counter
   end
 
-  def self.send_discounts account
+  def self.send_discounts(account)
     discounts = Discount.includes(:votes, :account, :members, :comments, :page_visits)
       .where("discounts.account_id" => account.id)
       .where('discounts.state' => "published")
-      .where("discounts.ends_at >= '#{Time.now}' or discounts.ends_at is null")
+      .where("discounts.ends_at >= '#{DateTime.now}' or discounts.ends_at is null")
 
-    if discounts.any?
-        NoticeMailer.discount_statistics(discounts, account) unless account.email.blank?
+    if discounts.any? && account.email.present?
+      StatisticsMailer.discount_statistics(discounts, account)
+      return true
+    else
+      return false
     end
   end
 
   def self.send_afisha_statistics
     accounts = Account.where("email is not null")
     managers = Role.all.map(&:user).map(&:account).uniq
+    counter = 0
     (accounts - managers).each do |account|
       begin
         if account.account_settings.afishas_statistics
-          send_afishas account
+          answer = send_afishas(account)
+          if answer
+            counter = counter + 1
+          end
         end
       rescue
         next
       end
     end
+    counter
   end
 
-  def self.send_afishas account
+  def self.send_afishas(account)
     afishas =  Afisha
       .actual
       .includes(:invitations, :comments, :visits, :showings)
       .where("afisha.user_id = '#{account.users.first.id}'")
       .where(:state => "published")
 
-    if afishas.any?
-      NoticeMailer.afisha_statistics(afishas, account) unless account.email.blank?
+    if afishas.any? && account.email.present?
+      StatisticsMailer.afisha_statistics(afishas, account)
+      return true
+    else
+      return false
     end
   end
 
--- lib/tasks/send_statistics.rake
+++ lib/tasks/send_statistics.rake
@@ -2,17 +2,26 @@
 
 desc "Send by email afisha and discout statistics to users"
 task :send_statistics => :environment do
+  # TODO uncomment everything!
   puts "="*10
 
   puts "Sending statistics. Please wait..."
 
-  puts "Sending afisha statistics. Please wait..."
+  puts "Sending afisha statistic. Please wait..."
 
-  SendEmailStatistics.send_afisha_statistics
+  count = SendEmailStatistics.send_afisha_statistics
 
-  puts "Sending discount statistics. Please wait..."
+  message = "Afisha statistic sending finished. Sended #{count} emails"
+  puts message
+  #Airbrake.notify(:error_class => "Rake Task", :error_message => message)
 
-  SendEmailStatistics.send_discount_statistics
+  puts "Sending discount statistic. Please wait..."
+
+  count = SendEmailStatistics.send_discount_statistics
+
+  message = "Discounts statistic sending finished. Sended #{count} emails"
+  puts message
+  #Airbrake.notify(:error_class => "Rake Task", :error_message => message)
 
   puts "="*10
 
