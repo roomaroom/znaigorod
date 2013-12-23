b36d63b4c0aaec6ee18154d8e5bff7458530b557
Nimdis <tsaazi@gmail.com>
2013-12-23 17:06:16 +0700

    now personal digest will send every day if user has updates. views ready for redesign

--- lib/send_personal_digest.rb
+++ lib/send_personal_digest.rb
@@ -4,20 +4,21 @@ class SendPersonalDigest
 
   class Digest
 
-    def self.collection_for_email(account)
+    def self.collection_for_email(account, period)
       @account = account
+      @period = period
       [
-        invitations,
-        private_messages,
-        comment_likes,
+        #invitations,
+        #private_messages,
+        #comment_likes,
         #comment_answers,
-        #material_comments,
+        material_comments,
       ].compact
     end
 
     def self.invitations
       invitations = []
-      invite = Invitation.where(['invited_id = ? and created_at > ?', @account.id, @account.last_visit_at])
+      invite = Invitation.where(['invited_id = ? and created_at > ?', @account.id, Time.zone.now - @period])
       invite.each do  |i|
         if i.invite_messages.where(:state => 'unread').any?
           invitations.push i
@@ -27,7 +28,7 @@ class SendPersonalDigest
     end
 
     def self.private_messages
-      @account.private_messages.where("state = 'unread' and created_at > ?", @account.last_visit_at)
+      @account.private_messages.where("state = 'unread' and created_at > ?", Time.zone.now - @period)
     end
 
     def self.comment_likes
@@ -36,7 +37,7 @@ class SendPersonalDigest
                                                 "messageable_type = 'Vote' and " +
                                                 "account_id = ? and " +
                                                 "created_at > ?",
-                                                @account.id, @account.last_visit_at)
+                                                @account.id, Time.zone.now - @period)
       notifications.each do |n|
         if n.messageable.voteable_type == "Comment"
           likes.push n.messageable
@@ -52,7 +53,7 @@ class SendPersonalDigest
                                                 "account_id = ? and " +
                                                 "created_at > ? and " +
                                                 "kind = 'reply_on_comment'",
-                                                @account.id, @account.last_visit_at)
+                                                @account.id, Time.zone.now - @period)
       notifications.each do |n|
         if n.messageable.parent.present?
           comments.push n.messageable
@@ -63,13 +64,15 @@ class SendPersonalDigest
 
     def self.material_comments
       comments = []
+
       notifications = NotificationMessage.where("state = 'unread' and " +
                                                 "messageable_type = 'Comment' and " +
                                                 "account_id = ? and " +
                                                 "created_at > ? and " +
-                                                "kind = 'new_comment'", @account.id, @account.last_visit_at)
+                                                "kind = 'new_comment'", @account.id, Time.zone.now - @period)
+      puts notifications.inspect
       notifications.each do |n|
-        if %w[Afisha, Discount].include?(n.messageable.commentable_type)
+        if %w[Afisha Discount].include?(n.messageable.commentable_type)
           comments.push n.messageable
         end
       end
@@ -80,9 +83,10 @@ class SendPersonalDigest
 
   def self.send
     visit_period = 1.day
-    Account.with_email.where('last_visit_at <= ?', Time.zone.now - visit_period).each do |account|
-      digest = Digest.collection_for_email(account)
-      puts 'here'
+#.where('last_visit_at <= ?', Time.zone.now - 1.day)
+    Account.with_email.each do |account|
+      digest = Digest.collection_for_email(account, visit_period)
+      puts digest.inspect
       PersonalDigestMailer.send_digest(account, digest) unless digest.flatten.blank?
     end
   end
