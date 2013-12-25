27d2c4689d0e47ed84e2af3848f182258a95007e
Nimdis <tsaazi@gmail.com>
2013-12-25 10:59:45 +0700

    mailer

--- app/mailers/site_digest_mailer.rb
+++ app/mailers/site_digest_mailer.rb
@@ -3,11 +3,10 @@ class SiteDigestMailer < ActionMailer::Base
   layout "notice_layout"
   add_template_helper(ImageHelper)
 
-  def send_digest
-    @digest = SendSiteDigest.made_digest(period)
-    Account.where("email is not null and last_visit_at <= #{DateTime.now - 1.week}").each do |account|
-      mail(:to => account.email, :subject => t("notice_mailer.new_notifications")).deliver!
-    end
+  def send_digest(account, digest)
+    @digest = digest
+    @type = "site_digest"
+    mail(:to => account.email, :subject => t("notice_mailer.site_digest")).deliver!
   end
 
 end
