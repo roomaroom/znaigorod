b208d4104fc149a53b7b665f376e25b2ac12ba0b
Nimdis <tsaazi@gmail.com>
2013-12-28 09:22:11 +0700

    added helpers to personal digest

--- app/mailers/personal_digest_mailer.rb
+++ app/mailers/personal_digest_mailer.rb
@@ -2,6 +2,7 @@ class PersonalDigestMailer < ActionMailer::Base
   default :from => "\"znaigorod.ru\" <#{Settings['mail']['from']}>"
   layout "notice_layout"
   add_template_helper(ImageHelper)
+  add_template_helper(EmailDigestHelper)
 
   def send_digest(account, digest)
     @digest = digest
