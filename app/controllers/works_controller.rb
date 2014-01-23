26b6c1cf8d0db0d61b26df3d5e0acb91b9d21a2f
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-01-23 12:30:14 +0700

    build_resource

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,4 +4,10 @@ class WorksController < ApplicationController
   actions :new, :create, :show
 
   belongs_to :contest
+
+  protected
+
+  def build_resource
+    super.account = current_user.try(:account)
+  end
 end
