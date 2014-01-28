47f5d7b0412cf4013900612ccf2eefa78ff3e6bb
Evgeny Lapin <lev@openteam.ru>
2014-01-28 13:46:37 +0700

    create sitemap symlinks after deploy:finalize_update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,13 @@
 require 'openteam/capistrano/recipes'
 require 'sidekiq/capistrano'
 
+namespace :sitemap do
+  desc 'Create symlinks to sitemap.xml and sitemap.xml.gz'
+  task :symlinks, :except => { :no_release => true } do
+    run "ln -nfs #{shared_path}/sitemaps/sitemap.xml #{deploy_to}/current/public/sitemap.xml"
+    run "ln -nfs #{shared_path}/sitemaps/sitemap.xml.gz #{deploy_to}/current/public/sitemap.xml.gz"
+  end
+  after "deploy:finalize_update", "sitemap:symlinks"
+end
+
 set :default_stage, 'production'
