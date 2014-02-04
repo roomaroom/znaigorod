b1b717377af999b2eeda61a121d88eb2be28b3f3
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-02-04 13:03:07 +0700

    Handle mailto links. Close #13

--- config/initializers/auto_html/links.rb
+++ config/initializers/auto_html/links.rb
@@ -1,8 +1,12 @@
+def skip_url?(url)
+  url =~ /#{Settings['app.url']}/ || url =~ /mailto:/
+end
+
 AutoHtml.add_filter(:external_links_attributes) do |html, options|
   fragment = Nokogiri::HTML.fragment(html.scrub)
 
   fragment.css('a').each do |a|
-    next if a['href'] =~ /#{Settings['app.url']}/
+    next if skip_url?(a['href'])
 
     a['target'] = '_blank'
     a['rel']    = 'nofollow'
@@ -11,12 +15,11 @@ AutoHtml.add_filter(:external_links_attributes) do |html, options|
   fragment.to_html
 end
 
-
 AutoHtml.add_filter(:external_links_redirect) do |html, options|
   fragment = Nokogiri::HTML.fragment(html.scrub)
 
   fragment.css('a').each do |a|
-    next if a['href'] =~ /#{Settings['app.url']}/
+    next if skip_url?(a['href'])
 
     a['href']   = AwayLink.to(a['href'])
   end
