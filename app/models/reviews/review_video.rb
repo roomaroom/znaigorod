e135c7143ecce1e97ad0d5a9ca11c6a1dd32a0fc
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-02-13 10:10:47 +0700

    Auto poster for video

--- app/models/reviews/review_video.rb
+++ app/models/reviews/review_video.rb
@@ -3,4 +3,10 @@ class ReviewVideo < Review
 
   validates :video_url, :presence => true
   validates :content, :length => { :maximum => 140 }
+
+  private
+
+  def set_poster
+    self.poster_image = Posts::ContentParser.new(video_url).poster
+  end
 end
