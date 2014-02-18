ce80b5f4dc2adda56334448d1903e8afc05f600e
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-02-18 11:22:30 +0700

    Rm posts: my/post_gallery_images_controller.rb & posts_controller.rb & post_decorator.rb

--- app/controllers/my/post_gallery_images_controller.rb
+++
@@ -1,5 +0,0 @@
-class My::PostGalleryImagesController < My::ApplicationController
-  defaults :resource_class => GalleryImage, :collection_name => :gallery_images, :instance_name => :gallery_image
-
-  belongs_to :post, :polymorphic => true, :optional => true
-end
