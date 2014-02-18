d4ad5e832b794ab4ce7d593d8cd5535b98d75ea7
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-02-18 11:45:27 +0700

    Rm posts: znaigorod_link filter

--- config/initializers/auto_html/znaigorod_link.rb
+++ config/initializers/auto_html/znaigorod_link.rb
@@ -3,6 +3,7 @@ require 'rexml/document'
 
 AutoHtml.add_filter(:znaigorod_link).with({}) do |text, options|
   attributes = Array(options).reject { |k,v| v.nil? }.map { |k, v| %{#{k}="#{REXML::Text::normalize(v)}"} }.join(' ')
+
   Rinku.auto_link(text, :all, attributes) do |url|
     if afisha_slug = url.match(Regexp.new("#{Settings[:app][:url]}/afisha/(\\S+)"))
       afisha = Afisha.published.find_by_slug(afisha_slug[1])
@@ -10,9 +11,9 @@ AutoHtml.add_filter(:znaigorod_link).with({}) do |text, options|
     elsif organization_slug = url.match(Regexp.new("#{Settings[:app][:url]}/organizations/(\\S+)"))
       organization = Organization.find_by_slug(organization_slug[1])
       organization ? organization.title : url
-    elsif post_slug = url.match(Regexp.new("#{Settings[:app][:url]}/posts/(\\S+)"))
-      post = Post.find_by_slug(post_slug[1])
-      post ? post.title : url
+    elsif review_slug = url.match(Regexp.new("#{Settings[:app][:url]}/reviews/(\\S+)"))
+      review = Review.find_by_slug(review_slug[1])
+      review ? review.title : url
     else
       url
     end
