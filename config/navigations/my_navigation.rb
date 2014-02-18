dad1351c3938c42acbe58b47b83ec9333bb89d18
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-02-18 12:20:54 +0700

    my navigation

--- config/navigations/my_navigation.rb
+++ config/navigations/my_navigation.rb
@@ -2,7 +2,7 @@
 
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
-    primary.item :my, 'Личный кабинет', my_root_path, highlights_on: /^\/my[\/]?$|^\/my\/afisha[\/]?$/ do |my_item|
+    primary.item :my, 'Личный кабинет', my_root_path, highlights_on: /my/ do |my_item|
       my_item.item :new_afisha, 'Создание мероприятия', new_my_afisha_path
       my_item.item :afisha, @afisha.title || 'Нет названия', my_afisha_path(@afisha) do |afisha_item|
         afisha_item.item :first_step, 'Описание', edit_step_my_afisha_path(@afisha, :first)
@@ -20,14 +20,13 @@ SimpleNavigation::Configuration.run do |navigation|
         afisha_item.item :showings, 'Расписание', edit_my_afisha_showing_path(@afisha, @showing) if @showing && @showing.persisted?
       end if @afisha && @afisha.persisted?
 
-      my_item.item :posts, 'Мои посты', my_posts_path do |posts|
-        posts.item :new, 'Добавление поста', new_my_post_path
-
-        posts.item :post, @post.title, my_post_path(@post) do |post|
-          post.item :edit, 'Редактирование поста', edit_my_post_path(@post)
-          post.item :poster, 'Постер', poster_my_post_path(@post)
-        end if @post.persisted?
-      end if @post
+      my_item.item :reviews, 'Мои обзоры', my_reviews_path do |reviews|
+        reviews.item :review, @review.title, my_review_path(@review) do |review|
+          review.item :edit, 'Редактирование обзора', edit_my_review_path(@review)
+          review.item :edit_poster, 'Изменение постера', poster_edit_my_review_path(@review)
+          review.item :add_images, 'Добавление фотографий', images_add_my_review_path(@review)
+        end if @review.persisted?
+      end if @review
 
       my_item.item :index, 'Добавление скидки', my_discounts_path
       my_item.item :new_discount, 'Добавление скидки', new_my_discount_path
