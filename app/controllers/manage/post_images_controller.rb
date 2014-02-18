018d5fcaaad7f597c928db5c140e2ecccafd8b9c
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-02-18 11:19:18 +0700

    Rm posts: manage/post_images_controller.rb & manage/posts_controller.rb

--- app/controllers/manage/post_images_controller.rb
+++
@@ -1,7 +0,0 @@
-class Manage::PostImagesController < Manage::ApplicationController
-  load_and_authorize_resource
-
-  actions :new, :create, :destroy, :update, :edit
-
-  belongs_to :post, :polymorphic => true, :optional => true
-end
