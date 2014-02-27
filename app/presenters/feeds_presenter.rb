cae3c97804eabcb7fa2402556e2460f14316d9ca
Nimdis <tsaazi@gmail.com>
2014-02-27 11:19:23 +0700

    fix of feeds of discounts

--- app/presenters/feeds_presenter.rb
+++ app/presenters/feeds_presenter.rb
@@ -22,10 +22,6 @@ class FeedsPresenter
     %w[accounts feeds].include?(@controller_name)
   end
 
-  def is_discount?(class_name)
-    %w[Coupon Discount Certificate].include?(class_name)
-  end
-
   def is_review?(class_name)
     %w[ReviewArticle ReviewPhoto ReviewVideo].include?(class_name)
   end
