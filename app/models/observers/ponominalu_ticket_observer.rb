16e8301a727291013baa83666d23299179861f20
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-02-28 14:14:42 +0700

    Show ponominalu link for posters

--- app/models/observers/ponominalu_ticket_observer.rb
+++ app/models/observers/ponominalu_ticket_observer.rb
@@ -2,4 +2,8 @@ class PonominaluTicketObserver < ActiveRecord::Observer
   def after_save(ticket)
     ticket.afisha.delay.reindex_showings
   end
+
+  def after_destroy(ticket)
+    ticket.afisha.delay.reindex_showings
+  end
 end
