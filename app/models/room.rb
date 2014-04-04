e01055150260b3ad30d00101109f069729f9a031
Ivan Puchkov <rake.eclipse@gmail.com>
2014-04-04 11:00:14 +0700

    room has many prices; ability to add prices for room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,10 +1,13 @@
 class Room < ActiveRecord::Base
   belongs_to :context, :polymorphic => true
-  attr_accessible :capacity, :context_id, :context_type, :description, :feature, :rooms_count, :title
+
+  attr_accessible :capacity, :context_id, :context_type, :description, :feature, :rooms_count, :title, :prices_attributes
+
+  has_many :prices, :as => :context, :class_name => 'DailyPrice', :dependent => :destroy
+
+  accepts_nested_attributes_for :prices, :allow_destroy => true
 
   include PresentsAsCheckboxes
 
-  presents_as_checkboxes :feature#,
-    #validates_presence: true,
-    #message: I18n.t('activerecord.errors.messages.at_least_one_value_should_be_checked')
+  presents_as_checkboxes :feature
 end
