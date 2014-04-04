ef32743d5c6fadeb1f81dea7b91dca946f5899e7
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-04-04 11:49:24 +0700

    Gsub title

--- app/models/promotions/place_item.rb
+++ app/models/promotions/place_item.rb
@@ -12,12 +12,24 @@ class PlaceItem < ActiveRecord::Base
   validates :ends_at, :presence => true
 
   def html
-    HTTParty.get(request_url).parsed_response.force_encoding('utf-8')
+    set_title_for(encoded_response)
   end
 
   private
 
   def request_url
     "#{Settings['app.url']}#{url}.promotion"
+  end
+
+  def response
+    HTTParty.get(request_url).parsed_response
+  end
+
+  def encoded_response
+    response.force_encoding('utf-8')
+  end
+
+  def set_title_for(html)
+    html.gsub '_title_', title
   end
 end
