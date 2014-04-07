5e9e1ef66463848f1e3bd5d32666911fc5d36cf7
Manin Iliy <bkmzvf@gmail.com>
2014-04-07 16:38:33 +0700

    actions added

--- app/controllers/manage/promotions_controller.rb
+++ app/controllers/manage/promotions_controller.rb
@@ -1,6 +1,8 @@
 class Manage::PromotionsController < Manage::ApplicationController
   load_and_authorize_resource
 
+  actions :all, :except => [:show]
+
   def index
     @promotions = Promotion.ordered
   end
