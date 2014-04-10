3573cdd82c2a63865a3a6706e5467964d200183f
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-04-10 12:35:27 +0700

    Global watermark

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -4,17 +4,23 @@ module ImageHelper
   def resized_image_url(url, width, height, options = { :crop => '!', :magnify => 'm', :orientation => 'n' })
     return if url.blank?
 
+    return url unless url.match /(\d+\/region\/\d+)|(\/files\/\d+\/\d+-\d+\/)/
+
     if url.match(/\d+\/region\/\d+/)
-      return url.gsub(/(\/files\/\d+\/region\/(\d+|\/){8})/) { "#{$1}#{width}-#{height}#{options[:crop]}/" }
+      modified_url = url.gsub(/(\/files\/\d+\/region\/(\d+|\/){8})/) { "#{$1}#{width}-#{height}#{options[:crop]}/" }
     end
 
     if url.match(/\/files\/\d+\/\d+-\d+\//)
       image_url, image_id, image_width, image_height, image_crop, image_filename = url.match(%r{(.*)/files/(\d+)/(?:(\d+)-(\d+)(\!)?/)?(.*)})[1..-1]
 
-      return "#{image_url}/files/#{image_id}/#{width}-#{height}#{options[:crop]}#{options[:magnify]}#{options[:orientation]}/#{image_filename}"
+      modified_url = "#{image_url}/files/#{image_id}/#{width}-#{height}#{options[:crop]}#{options[:magnify]}#{options[:orientation]}/#{image_filename}"
     end
 
-    url
+    watermark = options[:watermark].nil? ? true : options[:watermark]
+
+    modified_url.gsub! 'files', 'w/files' if watermark
+
+    modified_url
   end
 
   def image_direct_url(path)
