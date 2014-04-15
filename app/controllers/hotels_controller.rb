142335a730770d5092accbc791c412e6531fe193
Ivan Puchkov <rake.eclipse@gmail.com>
2014-04-15 15:24:02 +0700

    recreation centers index dirty

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,5 +1,6 @@
 class HotelsController <  ApplicationController
   def index
     @presenter = RoomsPresenter.new(:hotel, params)
+    @organizations = Organization.joins(:hotel).select {|o| o.hotel.rooms.count == 0 }
   end
 end
