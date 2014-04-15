5d69bca3d941ab1d5331121df831ac9f5a0af006
Ivan Puchkov <rake.eclipse@gmail.com>
2014-04-15 10:15:03 +0700

    recreation_center complex show

--- app/decorators/recreation_center_decorator.rb
+++ app/decorators/recreation_center_decorator.rb
@@ -4,7 +4,16 @@ class RecreationCenterDecorator < SuborganizationDecorator
   decorates :recreation_center
 
   def title
-    recreation_center.title? ? recreation_center.title : 'База отдыха'
+    if recreation_center.title?
+      recreation_center.title
+    else
+      case recreation_center.category
+      when 'Базы отдыха'
+        'База отдыха'
+      when 'Загородные дома'
+        'Загородный дом'
+      end
+    end
   end
 
   def htmlise_features_on_show
@@ -18,6 +27,10 @@ class RecreationCenterDecorator < SuborganizationDecorator
   def htmlise_offers_and_features_on_show
     content = htmlise_offers_on_show.to_s + htmlise_features_on_show.to_s
     h.content_tag(:ul, content.html_safe, class: :offers_and_features) if content. present?
+  end
+
+  def decorated_rooms
+    RoomDecorator.decorate rooms
   end
 end
 
