71d72d47845f39af500b4e3ef72edcaa45459382
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-05-19 16:52:48 +0700

    Add scopes for MainPageReview

--- app/models/reviews/main_page_review.rb
+++ app/models/reviews/main_page_review.rb
@@ -4,4 +4,8 @@ class MainPageReview < ActiveRecord::Base
   belongs_to :review
 
   scope :ordered, -> { order :position }
+  scope :actual, -> { where 'expires_at > ?', Time.zone.now }
+  scope :with_reviews, -> { where 'review_id IS NOT NULL' }
+
+  validates :expires_at, :presence => true
 end
