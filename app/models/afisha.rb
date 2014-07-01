d41021c4406ac123aa442c46b36064ea8e17ce12
Evgeniy Serykh <e.v.serykh@gmail.com>
2014-07-01 15:50:14 +0700

    More complex logic

--- app/models/afisha.rb
+++ app/models/afisha.rb
@@ -363,9 +363,7 @@ class Afisha < ActiveRecord::Base
   def can_be_deleted?
     return false if Time.zone.now > created_at + 3.hours
 
-    return false if ponominalu_tickets.any?
-
-    return false if tickets.any?
+    return false if tickets.joins(:copies).where('copies.state IN (?)', [:reserved, :sold]).any?
 
     true
   end
