7be1450bbd4c17c683c0526102a13721baf05244
Karmickoala904 <karmickoala904@gmail.com>
2014-07-14 12:13:08 +0700

    related_items controller

--- app/controllers/my/related_items_controller.rb
+++ app/controllers/my/related_items_controller.rb
@@ -1,16 +1,40 @@
-class RelatedItemsController < ApplicationController
-  def afishas
-    settings_from_cookie = {}
-    settings_from_cookie = Rack::Utils.parse_nested_query(cookie) if cookie.present?
+class My::RelatedItemsController < ApplicationController
 
-    @presenter = AfishaPresenter.new(settings_from_cookie.merge(params))
+  def afishas
+    searcher = HasSearcher.searcher(:showings).tap { |s|
+      s.paginate(page: page, per_page: per_page)
+      s.groups
+      s.send("order_by_creation")
+      s.actual
+    }
+
+    afisha_ids = searcher.group(:afisha_id_str).groups.map(&:value)
+    @related_afishas = Afisha.where(id: afisha_ids)
   end
 
   def organizations
+    searcher = HasSearcher.searcher(:organizations).tap { |s|
+      s.send("order_by_rating")
+      s.paginate(page: page, per_page: per_page)
+    }
 
+    @related_organizations = searcher.results
   end
 
   def reviews
+    searcher = HasSearcher.searcher(:reviews).tap { |s|
+      s.send("order_by_creation")
+      s.paginate(page: page, per_page: per_page)
+    }
+    @related_reviews = searcher.results
+  end
 
+  private
+  def per_page
+    6
+  end
+
+  def page
+    params[:page]
   end
 end
