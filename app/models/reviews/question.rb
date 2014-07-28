c1c95b6d853646d3dd6b3d5e5ec9e37db2ac868a
PeoneEr <peoneerko@yandex.ru>
2014-07-28 14:34:45 +0700

    add categories

--- app/models/reviews/question.rb
+++ app/models/reviews/question.rb
@@ -1,3 +1,10 @@
 class Question < Review
+  extend Enumerize
 
+  enumerize :categories,
+    :in => [:children, :interesting, :auto, :accidents, :crash, :animal, :cookery, :eighteen_plus, :humor, :hi_tech,
+            :leisure, :family, :lifehack, :economy, :complaints_book, :spirituality, :help,
+            :sex, :creativity, :handmade, :boiling, :good_news, :health, :sport, :policy, :studies, :abiturient, :advertisement],
+    :multiple => true,
+    :predicates => true
 end
