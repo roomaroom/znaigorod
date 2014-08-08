21510e3cca2b9a4cb67445e49731b5cba7cc9cba
PeoneEr <peoneerko@yandex.ru>
2014-08-08 16:18:54 +0800

    some changes for similar afisha

--- app/controllers/similar_afishas_controller.rb
+++ app/controllers/similar_afishas_controller.rb
@@ -3,11 +3,7 @@ class SimilarAfishasController < ApplicationController
     unless params[:title].empty?
       afisha_ids = HasSearcher.searcher(:showings, :q => params[:title]).actual.paginate(:page => 1, :per_page => 2).groups.group(:afisha_id_str).groups.map(&:value)
       @afishas = Afisha.where(:id => afisha_ids).decorate
-      unless @afishas.empty?
-        render :partial => "my/afishas/similar_afishas"
-      else
-        render :text => ""
-      end
+      render :partial => 'my/afishas/similar_afishas', :layout => false and return unless @afishas.empty? && request.xhr?
     end
   end
 end
