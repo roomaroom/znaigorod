99c3b9a07c79baf5a538cd406a19d19e572b9799
PeoneEr <peoneerko@yandex.ru>
2014-08-14 12:11:48 +0800

    more galleries

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -14,7 +14,7 @@ class WorksController < ApplicationController
   def show
     show! {
       @work.delay(:queue => 'critical').create_page_visit(request.session_options[:id], request.user_agent, current_user)
-      @photogalleries = Photogallery.all
+      @photogalleries = Photogallery.limit(5)
     }
   end
 
