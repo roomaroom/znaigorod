23d8ccf339507072d5f54a85fdde06eb0f182d1a
Karmickoala904 <karmickoala904@gmail.com>
2014-09-10 15:32:21 +0700

    migation fix

--- db/migrate/20140829090901_positive_activity.rb
+++ db/migrate/20140829090901_positive_activity.rb
@@ -2,11 +2,13 @@ class PositiveActivity < ActiveRecord::Migration
   def up
     add_column :organizations, :positive_activity_date, :datetime
 
+    Organization.reset_column_information
+
     without_activity = []
     Organization.where(status: :client).each do |org|
       activity = org.activities.where(state: :completed)
       if activity.present?
-        org.update_attribute(:positive_activity_date, activity.first.activity_at)
+        org.update_attributes(:positive_activity_date => activity.first.activity_at)
       else
         without_activity << org
       end
@@ -15,7 +17,7 @@ class PositiveActivity < ActiveRecord::Migration
     refresh_status = []
     without_activity.each do |org|
       if org.primary_organization.present?
-        org.update_attribute(:positive_activity_date, org.primary_organization.positive_activity_date)
+        org.update_attributes(:positive_activity_date => org.primary_organization.positive_activity_date)
       else
         refresh_status << org
       end
@@ -23,6 +25,11 @@ class PositiveActivity < ActiveRecord::Migration
 
     refresh_status.each do |org|
       org.update_attribute(:status, :fresh)
+    end
+
+    Organization.reindex
+    Dir["./app/models/suborganizations/*.rb"].each do |f|
+      File.basename(f, '.rb').classify.constantize.reindex
     end
 
   end
