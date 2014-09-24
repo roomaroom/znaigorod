35bfda1ff6af46659b3eb44ba03dd3909c06aa7e
Karmickoala904 <karmickoala904@gmail.com>
2014-09-24 17:02:03 +0700

    sti for works

---
+++ app/models/contests/work_video.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+class WorkVideo < Work
+end
+
+# == Schema Information
+#
+# Table name: works
+#
+#  id                 :integer          not null, primary key
+#  image_url          :text
+#  author_info        :string(255)
+#  context_id         :integer
+#  title              :string(255)
+#  description        :text
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  slug               :string(255)
+#  vk_likes           :integer
+#  account_id         :integer
+#  image_file_name    :string(255)
+#  image_content_type :string(255)
+#  image_file_size    :integer
+#  context_type       :string(255)
+#  rating             :float
+#
+
