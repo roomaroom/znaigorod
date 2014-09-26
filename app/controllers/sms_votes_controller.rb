4ee6ff7882d96d6d5723e071a88315bc366cd32f
Karmickoala904 <karmickoala904@gmail.com>
2014-09-26 16:21:52 +0700

    video contests votes create

--- app/controllers/sms_votes_controller.rb
+++ app/controllers/sms_votes_controller.rb
@@ -1,7 +1,14 @@
 class SmsVotesController < ApplicationController
   def index
-    File.open('getmvote.txt', 'w') { |f| f.write(params) }
-    Work.last.sms_votes.create
+    contest = Contest.where(sms_prefix: params[:prefix]).first
+    secret = contest.sms_secret.encode('utf-8') + params[:txt].encode('utf-8') + params[:tid].encode('utf-8') + params[:date].encode('utf-8')
+    md5_secret = Digest::MD5.new
+    md5_secret.update secret
+
+    if md5_secret.hexdigest == params[:sid]
+      work = contest.works.where(code: params[:txt].to_i).first
+      work.sms_votes.create(vote_date: params[:date], sid: params[:sid], sms_text: params[:txt], sms_id: params[:tid],prefix: params[:prefix] )
+    end
     render text: 'sms'
   end
 end
