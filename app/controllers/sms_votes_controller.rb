9aba88f6418496cf3dd5fe2a888ad5c0f339891e
Karmickoala904 <karmickoala904@gmail.com>
2014-09-26 14:52:29 +0700

    feed routes fix

--- app/controllers/sms_votes_controller.rb
+++ app/controllers/sms_votes_controller.rb
@@ -1,6 +1,7 @@
 class SmsVotesController < ApplicationController
   def index
     File.open('getmvote.txt', 'w') { |f| f.write(params) }
+    Work.last.sms_votes.create
     render text: 'sms'
   end
 end
