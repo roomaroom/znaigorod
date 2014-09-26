6fd01d6948f4f7c142fa2deb831b9c12d3140e1a
Karmickoala904 <karmickoala904@gmail.com>
2014-09-26 11:18:30 +0700

    typo fix

--- db/migrate/20140926035118_add_prefix_and_short_number_to_contests.rb
+++ db/migrate/20140926035118_add_prefix_and_short_number_to_contests.rb
@@ -1,6 +1,6 @@
 class AddPrefixAndShortNumberToContests < ActiveRecord::Migration
   def change
-    add_column :contests, :prefix, :string
+    add_column :contests, :sms_prefix, :string
     add_column :contests, :short_number, :integer
   end
 end
