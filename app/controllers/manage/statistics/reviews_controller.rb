0136855a2373b685b2c8a388a5770156a2826b16
Karmickoala904 <karmickoala904@gmail.com>
2014-10-01 13:43:23 +0700

    period filter for review statistic

--- app/controllers/manage/statistics/reviews_controller.rb
+++ app/controllers/manage/statistics/reviews_controller.rb
@@ -4,7 +4,19 @@ class Manage::Statistics::ReviewsController < Manage::ApplicationController
   has_scope :page, :default => 1
 
   def index
+    if params[:search] && params[:search]['starts_at'].present?
+      @starts_at = Time.zone.parse(params[:search]['starts_at']).beginning_of_day
+    else
+      @starts_at = Time.zone.today.beginning_of_month
+    end
+
+    if params[:search] && params[:search]['ends_at'].present?
+      @ends_at = Time.zone.parse(params[:search]['ends_at']).end_of_day
+    else
+      @ends_at = Time.zone.today.end_of_day
+    end
+
     author_ids = %w[4649, 6, 2303, 8581, 18960]
-    @reviews = Review.where(:account_id => author_ids, state: 'published').group_by(&:account_id)
+    @reviews = Review.where(:account_id => author_ids, state: 'published').where('created_at >= ? and created_at <= ?', @starts_at, @ends_at).group_by(&:account_id)
   end
 end
