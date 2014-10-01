ad1e78828092c99ea4af94d300fbb44ff0674ab3
PeoneEr <peoneerko@yandex.ru>
2014-10-01 09:58:10 +0700

    filters on payment statistic

--- app/controllers/manage/statistics/payments_controller.rb
+++ app/controllers/manage/statistics/payments_controller.rb
@@ -1,9 +1,16 @@
 class Manage::Statistics::PaymentsController < Manage::ApplicationController
   load_and_authorize_resource
+  helper_method :order_by
 
   has_scope :page, :default => 1
 
   def index
-    @payments = Payment.order('id DESC').page(params[:page]).per(20)
+    @payments = Payment.order('id desc').page(params[:page]).per(20).send(order_by)
+  end
+
+  protected
+
+  def order_by
+    params[:order_by] || 'every'
   end
 end
