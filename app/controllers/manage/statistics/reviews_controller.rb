a534e8f5b1e5228bcc6c7fc5177e24d998be2e85
Karmickoala904 <karmickoala904@gmail.com>
2014-10-01 15:54:46 +0700

    csv generation

--- app/controllers/manage/statistics/reviews_controller.rb
+++ app/controllers/manage/statistics/reviews_controller.rb
@@ -16,7 +16,28 @@ class Manage::Statistics::ReviewsController < Manage::ApplicationController
       @ends_at = Time.zone.today.end_of_day
     end
 
-    author_ids = %w[4649, 6, 2303, 8581, 18960]
-    @reviews = Review.where(:account_id => author_ids, state: 'published').where('created_at >= ? and created_at <= ?', @starts_at, @ends_at).group_by(&:account_id)
+    author_ids = %w[4649, 6, 2303, 8581, 18960, 14818]
+    @reviews = Review.where(:account_id => author_ids, state: 'published').where('created_at >= ? and created_at <= ?', @starts_at, @ends_at).where('type != ?', "Question").group_by(&:account_id)
+
+    respond_to do |format|
+      format.html
+      format.csv {
+        generated_csv = CSV.generate do |csv|
+          csv << ["Автор", "Название обзора", "Количество символов", "Количестов просмотров"]
+          @reviews.each do |author, review|
+            review.each do |attr|
+              csv << [Account.find(author), attr.title, attr.content? ? attr.content.size : 0, attr.page_visits.count]
+            end
+            csv << ["", "", total_review_count(review), ""]
+          end
+        end
+        send_data generated_csv
+      }
+    end
+  end
+
+  private
+  def total_review_count(r)
+    r.map{ |rw| rw.content ? rw.content.size : 0 }.sum
   end
 end
