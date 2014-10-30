56fd8275480f946081d329a5e906385eebea2e43
Karmickoala904 <karmickoala904@gmail.com>
2014-10-30 15:03:35 +0600

    dicount statistic

--- app/controllers/manage/statistics/discounts_controller.rb
+++ app/controllers/manage/statistics/discounts_controller.rb
@@ -18,4 +18,25 @@ class Manage::Statistics::DiscountsController < Manage::ApplicationController
     discounts = Discount.where(:id => @groups.map(&:value)).order('id DESC')
     @grouped_discounts = GroupedDiscounts.new(discounts).grouped
   end
+
+  def discount_statistic
+    if params[:search] && params[:search]['starts_at'].present?
+      @starts_at = Time.zone.parse(params[:search]['starts_at']).beginning_of_day
+    else
+      @starts_at = Time.zone.today.beginning_of_month
+    end
+
+    if params[:search] && params[:search]['ends_at'].present?
+      @ends_at = Time.zone.parse(params[:search]['ends_at']).end_of_day
+    else
+      @ends_at = Time.zone.today.end_of_day
+    end
+
+    author_ids = %w[4649, 6, 2303, 8581, 18960, 14818, 15669]
+
+    @discounts = Discount.where(:account_id => author_ids, state: 'published')
+                .where('created_at >= ? and created_at <= ?', @starts_at, @ends_at)
+                .order('created_at DESC')
+                .group_by(&:account_id)
+  end
 end
