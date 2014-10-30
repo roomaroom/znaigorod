c3f4302aec9ddeb1bed29749892f6958def64fe7
PeoneEr <peoneerko@yandex.ru>
2014-10-30 14:24:27 +0600

    rel=nofollow and noindex for some blocks

--- app/presenters/link.rb
+++ app/presenters/link.rb
@@ -4,7 +4,7 @@ class Link
   include ActiveAttr::QueryAttributes
   include ActiveAttr::MassAssignment
   include ActionView::Helpers::UrlHelper
-  attr_accessor :title, :kind, :selected, :html_options, :url, :disabled
+  attr_accessor :title, :kind, :selected, :html_options, :url, :disabled, :nofollow
   attribute :current
 
   def initialize(options)
@@ -13,6 +13,10 @@ class Link
     self.html_options[:class] ||= ""
     if disabled
       html_options[:class].blank? ?  html_options[:class] = "disabled" : html_options[:class] += " disabled"
+    end
+
+    if nofollow
+      html_options[:rel] = 'nofollow'
     end
   end
 
