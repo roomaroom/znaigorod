e50b47cb89df40f291330f70e4e73ca332e08177
Karmickoala904 <karmickoala904@gmail.com>
2014-11-06 15:26:04 +0600

    fix smart promotion for /discounts/discount/

--- lib/promotion_finder.rb
+++ lib/promotion_finder.rb
@@ -13,7 +13,6 @@ class PromotionFinder
     nil
   end
 
-  private
   def split_url
     @splited_url = promotion_url.try(:split, '/') || ['/afisha']
     @splited_url = @splited_url.drop(1)
@@ -27,7 +26,7 @@ class PromotionFinder
     split_url
 
     while url_array.length <= splited_url_length do
-      url_array << url_array.last.gsub("/#{splited_url.pop}","")
+      url_array << url_array.last.gsub(/\/#{splited_url.pop}$/,'')
     end
     url_array
   end
