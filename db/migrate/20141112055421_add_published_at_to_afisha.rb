826ce0faa59ed297fb262d025a8a4ea05bad0fdd
PeoneEr <peoneerko@yandex.ru>
2014-11-12 15:12:22 +0600

    remove migration

--- db/migrate/20141112055421_add_published_at_to_afisha.rb
+++
@@ -1,9 +0,0 @@
-class AddPublishedAtToAfisha < ActiveRecord::Migration
-  def change
-    add_column :afisha, :published_at, :datetime
-
-    Afisha.where(state: 'published').each do |afisha|
-      afisha.published_at = afisha.updated_at
-    end
-  end
-end
