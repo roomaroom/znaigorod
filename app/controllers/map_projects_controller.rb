8e2b239c9a06a01238944670383d787708475453
PeoneEr <peoneerko@yandex.ru>
2014-12-02 11:50:18 +0600

    reviews in map_project

--- app/controllers/map_projects_controller.rb
+++ app/controllers/map_projects_controller.rb
@@ -4,4 +4,11 @@ class MapProjectsController < ApplicationController
   actions :index, :show
 
   layout 'map_projects'
+
+  def show
+    show! {
+      @presenter = ReviewsPresenter.new(category: 'newyear', per_page: 100)
+      @reviews = @presenter.decorated_collection
+    }
+  end
 end
