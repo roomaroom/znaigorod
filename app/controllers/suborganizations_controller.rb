f5e6aa92c5d4435a8700c6e624c459c043994314
Karmickoala904 <karmickoala904@gmail.com>
2015-01-21 17:04:13 +0600

    per_page for clients organizations

--- app/controllers/suborganizations_controller.rb
+++ app/controllers/suborganizations_controller.rb
@@ -7,8 +7,9 @@ class SuborganizationsController < ApplicationController
       format.html {
         cookie = cookies['_znaigorod_organization_list_settings'].to_s
         settings_from_cookie = {}
+        per_page = Organization.where(status: :client).count
         settings_from_cookie = Rack::Utils.parse_nested_query(cookie) if cookie.present?
-        @presenter = klass.new(settings_from_cookie.merge(params))
+        @presenter = klass.new(settings_from_cookie.merge(params.merge(per_page: per_page)))
         if request.xhr?
           render partial: 'organizations/organizations_posters', layout: false and return unless (@presenter.collection.last_page? && params[:not_client_page].present? )
           render partial: 'organizations/not_client_posters', layout: false
