0e2ec2a9865704e096d1b01168657471602b9ba4
PeoneEr <peoneerko@yandex.ru>
2015-01-27 15:01:04 +0600

    fix http://errors.openteam.ru/apps/26f44639733967184d7ccc54c68ef751/problems/54c0b25907213a2346028c4c

--- app/controllers/away_controller.rb
+++ app/controllers/away_controller.rb
@@ -2,7 +2,7 @@ class AwayController < ActionController::Base
   def go
     path = request.fullpath.try :gsub, '/away?to=', ''
 
-    link = LinkCounter.where(link: path).first || LinkCounter.create(link_type: :external, name: path, link: path)
+    link = LinkCounter.where(link: path.truncate(254)).first || LinkCounter.create(link_type: :external, name: path.truncate(254), link: path.truncate(254))
     link.increment! :count
 
     redirect_to path || root_path
