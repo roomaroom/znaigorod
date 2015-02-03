07ee492f8e8856bd3507fe4ab6e40611d109b0d0
PeoneEr <peoneerko@yandex.ru>
2015-02-03 14:44:55 +0600

    some fixes for placemarks

--- app/controllers/manage/map_projects_controller.rb
+++ app/controllers/manage/map_projects_controller.rb
@@ -2,8 +2,8 @@ class Manage::MapProjectsController < Manage::ApplicationController
   load_and_authorize_resource
 
   def show
-    show!{
-      @map_placemarks = MapPlacemark.offset(82) #TODO: как-то фиксить ннада
+    show! {
+      @map_placemarks = MapProject.find(params[:id]).map_layers.map(&:map_placemarks).flatten.uniq
     }
   end
 end
