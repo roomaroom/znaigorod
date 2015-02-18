52fbaecc9dd8feb6b966eadf81452d4db13f6c5a
Karmickoala904 <karmickoala904@gmail.com>
2015-02-18 11:21:55 +0600

    hotel breadcrumbs

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -3,8 +3,8 @@ class HotelsController <  ApplicationController
 
   def index
     add_breadcrumb "Все организации", organizations_path
-    add_breadcrumb I18n.t(params[:kind]), send("#{params[:kind].pluralize}_path") if params[:kind]
-    add_breadcrumb params[:categories].first.mb_chars.capitalize, send("#{params[:kind].pluralize}_#{params[:categories].first.from_russian_to_param.pluralize}_path") if params[:categories]
+    add_breadcrumb "Базы отдыха и гостиницы", hotels_path
+    add_breadcrumb params[:categories].first.mb_chars.capitalize, send("hotels_#{params[:categories].first.from_russian_to_param.pluralize}_path") if params[:categories]
     @presenter = RoomsPresenter.new(:hotel, params.merge(per_page: per_page))
     if request.xhr?
       render partial: 'without_rooms_list_view', layout: false and return if view_type == 'list'
