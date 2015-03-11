63922b72a4f1327ca5e4e4aa507bfc3e053c7be2
Karmickoala904 <karmickoala904@gmail.com>
2015-03-11 14:32:21 +0600

    return car_service_center

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -103,7 +103,7 @@ class Organization < ActiveRecord::Base
   end
 
   def self.available_suborganization_kinds
-    Dir[Rails.root.join('app/models/suborganizations/*.rb')].map { |f| f.split('/').last.gsub '.rb', '' } - ['shop', 'car_service_center']
+    Dir[Rails.root.join('app/models/suborganizations/*.rb')].map { |f| f.split('/').last.gsub '.rb', '' } - ['shop']
   end
 
   available_suborganization_kinds.each do |kind|
