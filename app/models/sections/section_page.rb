3afde19b697a7e043db85aadfb099a50ebc01777
PeoneEr <peoneerko@yandex.ru>
2015-04-02 09:45:03 +0600

    ckeditor - начало; удаление постера; удаление лишних столбцов

--- app/models/sections/section_page.rb
+++ app/models/sections/section_page.rb
@@ -3,14 +3,10 @@ class SectionPage < ActiveRecord::Base
 
   validates_presence_of :title, :content
 
-  attr_accessible :title, :content, :poster
+  attr_accessible :title, :content, :poster, :vfs_path, :poster_image
 
   has_many :gallery_images,        :as => :attachable,     :dependent => :destroy
   belongs_to :section
-
-  before_save :store_cached_content_for_show,
-              :store_cached_content_for_index,
-              :set_poster
 
   has_attached_file :poster_image, storage: :elvfs, elvfs_url: Settings['storage.url']
 
--- config/routes/manage.rb
+++ config/routes/manage.rb
@@ -158,7 +158,9 @@ Znaigorod::Application.routes.draw do
       resources :organizations,  :only => [:new, :create, :destroy]
 
       resources :sections, :only => [:show, :destroy, :edit, :update] do
-        resources :section_pages, :except => [:index, :show]
+        resources :section_pages, :except => [:index, :show] do
+          delete 'destroy_poster', :on => :member, :as => :destroy_poster
+        end
       end
     end
 
