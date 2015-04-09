f67346af4eacab7e46cd20decc0faa453c4a5330
PeoneEr <peoneerko@yandex.ru>
2015-04-09 10:36:34 +0600

    правильные cookie; небольшой фикс баннеров Севастополя

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
-
-Znaigorod::Application.config.session_store :cookie_store, key: Settings['cookie.key'], :domain => :all
+#
+Znaigorod::Application.config.session_store :cookie_store, key: Settings['app.cookie.key'], :domain => :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
